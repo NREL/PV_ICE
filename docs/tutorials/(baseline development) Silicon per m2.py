7f012eccc4ee9b59b2c89782131d29326b94bf1d
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2020-09-15 17:05:35 -0600

    AddingSiliconbaseline
    Placeholder and beginings of silicon baseline file with documentation tutorial like glass.

---
+++ docs/tutorials/(baseline development) Silicon per m2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # (baseline development) Silicon per M2 Calculations
+
+# This journal documents the methods and assumptions made to create a baseline material file for silicon.
+
+# ## Mass per M2
+
+# The mass of silicon contained in a PV module is dependent on the size, thickness and number of cells in an average module. Since there is a range of sizes and number of cells per module, we will attempt a weighted average. These weighted averages are based on ITRPV data, which goes back to 2010.
+
+# In[ ]:
+
+
+
+
+
+# ## Virgin Material Efficiency
+
+# In[ ]:
+
+
+
+
