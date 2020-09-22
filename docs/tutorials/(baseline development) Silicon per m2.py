49c884c2764ddbd43db36d7cf9d09a44d3a16973
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2020-09-22 12:19:45 -0600

    Silicon Baseline Update
    got interpolations working for marketshare datasets

--- docs/tutorials/(baseline development) Silicon per m2.py
+++ docs/tutorials/(baseline development) Silicon per m2.py
@@ -9,7 +9,7 @@
 
 # The mass of silicon contained in a PV module is dependent on the size, thickness and number of cells in an average module. Since there is a range of sizes and number of cells per module, we will attempt a weighted average. These weighted averages are based on ITRPV data, which goes back to 2010, Fraunhofer data back to 1990, and 
 
-# In[114]:
+# In[32]:
 
 
 import numpy as np
@@ -23,7 +23,7 @@ density_si = 2.3290 #g/cm^3 from Wikipedia of Silicon (https://en.wikipedia.org/
 
 # A Fraunhofer report indicates that in 1990, wafers were 400 micron thick, decreasing to the more modern 180 micron thickness by 2008. ITRPVs back to 2010 indicate that 156 mm x 156mm was the standard size wafer through 2015.
 
-# In[115]:
+# In[33]:
 
 
 #weighted average for wafer size 2016 through 2030
@@ -34,48 +34,64 @@ wafer2016avg = 0.9*wafer2016mcsi + 0.1*wafer2016monosi
 print("Average Wafer size in 2016 was", wafer2016avg, "cm on a side")
 
 
-# In[116]:
+# In[43]:
 
 
 #now lets try to do this for 2019 through 2030 all at once with dataframes
 #taking the average of the ranges specified in ITRPVs
 
-#first we input the market share data for mcSi and monoSi --> should turn this into a csv
-marketshare_mcSi = {'share156':[0.9,0.37,0.1,0,0,0,0,0,0],
-                   'share156.75':[0.1,0.63,0.9,0.7,0.3,0.08,0.03,0.01,0],
-                   'share157.75':[0,0,0,0.24,0.35,0.30,0.15,0.04,0.05],
-                   'share163':[0,0,0.02,0.04,0.2,0.32,0.32,0.3,0.2],
-                   'share166up':[0,0,0,0.02,0.15,0.3,0.4,0.65,0.75]}
-marketshare_monoSi = {'share156':[0.55,0.21,0.08,0,0,0,0,0,0],
-                   'share156.75':[0.45,0.78,0.9,0.68,0.35,0.15,0,0,0],
-                   'share157.75':[0,0,0,0.23,0.3,0.3,0.15,0.05,0.01],
-                   'share163':[0,0.01,0.02,0.07,0.14,0.18,0.15,0.08,0.06],
-                   'share166up':[0,0,0,0.02,0.21,0.37,0.7,0.87,0.93]}
-#then shove them into panda dataframes
-dfmarketshare_mcSi = pd.DataFrame(marketshare_mcSi, index = ['2016','2017','2018','2019','2020','2022','2024','2027','2030'])
-dfmarketshare_monoSi = pd.DataFrame(marketshare_monoSi, index = ['2016','2017','2018','2019','2020','2022','2024','2027','2030'])
-    #it is necessary to put the year as the row index 
-    #because otherwise it gets added in the aggregation of columns
-    #later when we obtain the average cell size
+#first we input the market share data for mcSi and monoSi, read in from csv
+cwd = os.getcwd() #grabs current working directory
+mrktshr_cellsize = pd.read_csv(cwd+"/../../CEMFC/baselines/SupportingMaterial/MarketShare_CellSize.csv", index_col='Year')
+mrktshr_cellsize /=100 #turn whole numbers into decimal percentages
+#print(mrktshr_cellsize)
+
+#then split them into two dataframes for later computations
+dfmarketshare_mcSi = mrktshr_cellsize.filter(regex = 'mcSi')
+dfmarketshare_monoSi = mrktshr_cellsize.filter(regex = 'monoSi')
+#adjust column names for matching computation later
+dfmarketshare_mcSi.columns = ['share156','share156.75','share157.75','share163','share166up']
+dfmarketshare_monoSi.columns = ['share156','share156.75','share157.75','share163','share166up']
+
 print(dfmarketshare_mcSi)
 print(dfmarketshare_monoSi)
-#columns = dfmarketshare_monoSi.columns
-#print(columns)
 
 
-# In[117]:
+# Interpolate marketshare for missing years in ITRPV 2020 predictions
+# ----
+# choosing to interpolate market share of different sizes rather than cell size because this should be more basedin technology - i.e. crystals only grow certain sizes. Additionally, it is more helpful to understand the impact silicon usage by keeping cell size and marketshare seperate.
+
+# In[57]:
+
+
+#interpolate for missing marketshare data
+##the interpolate function returns a view of the df, doesn't modify the df itself
+##therefore you have to set the old df, or a new one = the df.interpolate function
+dfmarketshare_mcSi=dfmarketshare_mcSi.interpolate(method='linear',axis=0,limit=2,limit_area='inside')
+dfmarketshare_monoSi=dfmarketshare_monoSi.interpolate(method='linear',axis=0,limit=2,limit_area='inside')
+
+#fill remaining NaN/outside with 0 (i.e., no market share)
+dfmarketshare_mcSi=dfmarketshare_mcSi.fillna(0.0)
+dfmarketshare_monoSi=dfmarketshare_monoSi.fillna(0.0)
+
+print(dfmarketshare_mcSi)
+print(dfmarketshare_monoSi)
+
+
+# In[58]:
 
 
 #multiply each marketshare dataframe column by it's respective size
-#dfmarketshare_mcSi.share156 *=156 #this is a manual way to multiply each column by its respective size
+#dfmarketshare_mcSi.share156 *=156 #this is a slow way to multiply each column by its respective size
 
 cellsizes = {'share156':156,
             'share156.75':156.75,
             'share157.75':157.75,
-            'share163':163,
-            'share166up':166} #dictionary of the average cell dimension for each market share option
+            'share163':163.875,
+            'share166up':166} #dictionary of the average cell dimension for each market share bin (ITRPV 2020)
 
 #multiply cell dimensions by their market share to get a weighted average
+##this is where the column names needed to match
 df_scalecell_mcSi = dfmarketshare_mcSi.mul(cellsizes,'columns')
 df_scalecell_monoSi = dfmarketshare_monoSi.mul(cellsizes,'columns')
 
@@ -83,7 +99,7 @@ print(df_scalecell_mcSi)
 print(df_scalecell_monoSi)
 
 
-# In[138]:
+# In[59]:
 
 
 #now add the columns together to get the weighted average cell size for each year for each technology
@@ -96,21 +112,16 @@ df_avgcell = pd.concat([df_avgcell_monoSi,df_avgcell_mcSi], axis=1) #concatinate
 df_avgcell.columns = ['monoSi','mcSi'] #name the columns
 print(df_avgcell)
 
-#extrapolate data back to 1980, assuming 100% marketshare of 156 mm cell size
-#i.e., input 156 for all years prior to 2016 for both technologies
-
 
 # Now we have an average cell dimension for mc-Si and mono-Si for 2016 through 2030. Next, we apply the marketshare of mc-Si vs mono-Si to get the average cell dimension for the year. Market share of mc-Si vs mono-Si is taken from LBNL "Tracking the Sun" report (warning: this is non-utility scale data i.e. <5MW, and is from 2002-2018), from Mints 2019 SPV report, from ITRPVs, and old papers (Costello & Rappaport 1980, Maycock 2003 & 2005).
 
-# In[134]:
+# In[60]:
 
 
 #read in a csv that was copied from CE Data google sheet
 cwd = os.getcwd() #grabs current working directory
-techmarketshare = pd.read_csv(cwd+"/../../CEMFC/baselines/SupportingMaterial/ModuleType_MarketShare.csv")
+techmarketshare = pd.read_csv(cwd+"/../../CEMFC/baselines/SupportingMaterial/ModuleType_MarketShare.csv",index_col='Year')
 #this file path navigates from current working directory back up 2 folders, and over to the csv
-techmarketshare.index = techmarketshare['Year'] #make the year column the row index
-del techmarketshare['Year'] #delete the year column
 techmarketshare /=100 #turn whole numbers into decimal percentages
 techmarketshare['Year'] = techmarketshare.index #add the year back in for later merge
 print(techmarketshare)
@@ -118,7 +129,7 @@ print(techmarketshare)
 
 # #### create a harmonization of annual market share, and interpolate
 
-# In[136]:
+# In[62]:
 
 
 # first, create a single value of tech market share in each year or NaN
@@ -146,6 +157,20 @@ print(est_mrktshrs)
 del est_mrktshrs['Total']
 
 #interpolate marketshares for each year
+
+
+# In[66]:
+
+
+#Interpolate for marketshare NaN values
+est_mrktshrs['mcSi'][1980]=0.0
+est_mrktshrs = est_mrktshrs.interpolate(method='linear',axis=0,limit_area='inside')
+
+#sanity check of market share data - does it add up?
+est_mrktshrs['Total'] = est_mrktshrs.monoSi+est_mrktshrs.mcSi
+print(est_mrktshrs)
+#Warning: 2002, 10% of the silicon marketshare was "other", including amorphous, etc.
+del est_mrktshrs['Total']
 
 
 # In[146]:
