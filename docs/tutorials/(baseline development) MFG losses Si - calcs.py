a3f37dba1ddbc938c8fd3af49ffb130f17e31d0d
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2020-10-09 11:34:16 -0600

    Begin MFG losses for Si Journal
    created a journal for MFG losses for Si to contribute to baseline files. did some branch nonsense.

---
+++ docs/tutorials/(baseline development) MFG losses Si - calcs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Manufacturing Efficiency Estimation Calculations
+
+# This notebook covers the calculations used to estimate the manufacturing efficiency of silicon for mono and mc Si modules. This data will be utilized in the baseline files for PV DEMICE.
+
+# In[ ]:
+
+
+
+
