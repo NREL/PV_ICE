99b3ea5c4af1a58cc92383dcc24395fd0c7577bc
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2020-10-14 11:49:19 -0600

    Continued work on MFG loss calcs
    almost completed 2010-2017 based on kerf loss calcs. Need to add market share of slurry vs diamond wire to correctly balance g/wafer. Will extract hat from ITRPVs, then input as csv for weighting like in silicon per m2 baseline.

--- docs/tutorials/(baseline development) MFG losses Si - calcs.py
+++ docs/tutorials/(baseline development) MFG losses Si - calcs.py
@@ -7,7 +7,7 @@
 # 
 # NOTE: You must run Silicon per m2 journal before running this one, as the calculations depend on grams of silicon per cell.
 
-# In[2]:
+# In[62]:
 
 
 import numpy as np
@@ -18,7 +18,7 @@ plt.rcParams.update({'font.size': 22})
 plt.rcParams['figure.figsize'] = (12, 8)
 
 
-# In[5]:
+# In[63]:
 
 
 #read in supporting csv files
@@ -47,7 +47,7 @@ wafer_thickness = pd.read_csv(cwd+"/../../PV_DEMICE/baselines/SupportingMaterial
 # 
 #             [(Wafer Thickness + Kerf Loss)/Wafer Thickness] * 100 = % mfg "inefficeincy"
 
-# In[14]:
+# In[64]:
 
 
 #There are missing data in wafer thickness, so we will interpolate linearly for missing years
@@ -64,7 +64,7 @@ df_thick_kerf = pd.concat([wafer_thick,kerf_loss], axis=1) #concatinate on the c
 
 # Because slurry and diamond have significantly different kerf losses, we will keep these seperate for as long as possible before averaging.
 
-# In[31]:
+# In[87]:
 
 
 df_thick_kerf['slurry_eff'] = df_thick_kerf['wafer_thickness']/(df_thick_kerf['wafer_thickness']+df_thick_kerf['slurry'])
@@ -78,16 +78,54 @@ df_thick_kerf['diamond_ineff'] = (df_thick_kerf['wafer_thickness']+df_thick_kerf
 
 # As a reality check, and because we have data for 2017 of both kerf loss and "utilization", we can check how good a proxy kerf loss is for utilization of input material.
 
-# In[30]:
+# In[83]:
 
 
-#First, multiply the proxy mfg efficiency by the averaged g/cell of silicon (as calculated by "Silicon per m2")
+#Multiply the proxy mfg efficiency by the averaged g/cell of silicon (as calculated by "Silicon per m2")
 check2017_slurry = (si_g_percell['Si_gpercell'][2017])*(df_thick_kerf['slurry_ineff'][2017])
 check2017_diamond = (si_g_percell['Si_gpercell'][2017])*(df_thick_kerf['diamond_ineff'][2017])
 
-#utilize_gperwafer_raw
-print(check2017_slurry)
-print(check2017_diamond)
+#extract ITRPV 2017 values for g/wafer utilization, and compute their mean
+itrpv2017_slurrys = [utilize_gperwafer_raw['mc-Si-slurry'][2017],utilize_gperwafer_raw['mono-Si-slurry'][2017]]
+itrpv2017_diamonds = [utilize_gperwafer_raw['mc-Si-diamond'][2017],utilize_gperwafer_raw['Mono-Si-diamond'][2017]]
+itrpv2017_slurry = np.mean(itrpv2017_slurrys)
+itrpv2017_diamond = np.mean(itrpv2017_diamonds)
+
+#Compare this calculated value to ITRPV value in 2017
+#print("In 2017, the calculated silicon utilization for the slurry cut is ",check2017_slurry, 
+      #"g/wafer, and diamond wire cut is",check2017_diamond, "g/wafer.")
+#print("In comparison, the 2017 ITRPV average silicon utilization for slurry cut is",itrpv2017_slurry, 
+      #"g/wafer, and diamond wire cut is", itrpv2017_diamond, "g/wafer.")
+
+#Compare in a printed out table, since this is hard to see
+data = {'Slurry':[check2017_slurry,itrpv2017_slurry],'Diamond':[check2017_diamond,itrpv2017_diamond]}
+gperwafer_compare = pd.DataFrame(data, index= ['Calculated','ITRPV'])
+print(gperwafer_compare)
+
+
+# In both cases, the calculated silicon utilization is less than the ITRPV survey collected data. This is unsurprising because the ITRPV utilization accounts for more than kerf loss (which is the basis for the calculated value). Therefore, we will compute the difference, and use this as a factor to add to all calculated values based on kerf loss alone.
+
+# In[80]:
+
+
+#gperwafer_compared = gperwafer_compare.append(gperwafer_compare.diff())
+gperwafer_compared = gperwafer_compare.diff()
+#print(gperwafer_compared)
+slurry_factor = gperwafer_compared['Slurry']['ITRPV']
+diamond_factor = gperwafer_compared['Diamond']['ITRPV']
+print("Add ", slurry_factor, "to slurry cut wafer values, and ", diamond_factor, "to diamond wire cut values.")
+
+
+# In[89]:
+
+
+#Multiply the proxy mfg efficiency by the averaged g/cell of silicon (as calculated by "Silicon per m2")
+#and add the 2017 computed factors
+slurry_gpw = (si_g_percell['Si_gpercell']*df_thick_kerf['slurry_ineff'])+ slurry_factor
+diamond_gpw = (si_g_percell['Si_gpercell']*df_thick_kerf['diamond_ineff'][2017])+ diamond_factor 
+#these are now pd.series, need to create a single value based on marketshare of diamond vs slurry
+#we know slurry was around until 2017, last year of ITRPV slurry cut data
+#and in 2010, diamond cut isn't mentioned only slurry
 
 
 # ## 2017 through 2030
