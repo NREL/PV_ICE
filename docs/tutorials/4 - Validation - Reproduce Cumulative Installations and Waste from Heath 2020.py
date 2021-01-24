20607d86a4e0de6d028343b2d77239079353871d
Silvana Ayala <silvana.ayala@nrel.gov>
2021-01-24 13:46:06 -0700

    Validation Journal cleanup

--- docs/tutorials/4 - Validation - Reproduce Cumulative Installations and Waste from Heath 2020.py
+++ docs/tutorials/4 - Validation - Reproduce Cumulative Installations and Waste from Heath 2020.py
@@ -11,8 +11,8 @@
 # ![Input from IRENA_projections](../images_wiki/IRENA_projections.PNG)
 # 
 # Notes on IRENA Data:
-# - Installation Data < 2010 from D. Jordan
-# - Installation data >= 2010 from IRENA report
+# - Installation Data < 2010 from D. Jordan (Values too low to digitize properly)
+# - Installation data >= 2010 from IRENA report (digitized from plot)
 # 
 # 
 # Other considerations:
@@ -53,52 +53,37 @@ plt.rcParams.update({'font.size': 22})
 plt.rcParams['figure.figsize'] = (12, 5)
 
 
-# ## REPAIR
+# ## PV ICE
 
-# In[42]:
+# In[4]:
 
 
 r1 = PV_ICE.Simulation(name='Simulation1', path=testfolder)
-r1.createScenario(name='Garvin_2020', file=r'..\baselines\baseline_modules_World_Irena.csv')
+r1.createScenario(name='Garvin_2020', file=r'..\baselines\baseline_modules_World_Irena_2019.csv')
 r1.scenario['Garvin_2020'].addMaterial('glass', file=r'..\baselines\baseline_material_glass.csv')
 
-r1.createScenario(name='PV_ICE_default', file=r'..\baselines\baseline_modules_World.csv')
-r1.scenario['PV_ICE_default'].addMaterial('glass', file=r'..\baselines\baseline_material_glass.csv')
+
+# In[5]:
 
 
-# In[43]:
+print(r1.scenario.keys())
+print("")
+print(r1.scenario['Garvin_2020'].data.keys())
+print("")
+print(r1.scenario['Garvin_2020'].material['glass'].materialdata.keys())
 
 
-r1.scenario['Garvin_2020'].data.keys()
+# Plot same plot from Garvin's paper from digitized data input
+
+# In[6]:
 
 
-# In[44]:
-
-
-plt.plot(r1.scenario['Garvin_2020'].data['year'], r1.scenario['Garvin_2020'].data['new_Installed_Capacity_[MW]']/1000, 'r', label = 'Irena Digitized')
-plt.plot(r1.scenario['PV_ICE_default'].data['year'], r1.scenario['PV_ICE_default'].data['new_Installed_Capacity_[MW]']/1000, 'g', label = 'World Baseline')
-plt.legend()
-plt.xlabel('Year')
-plt.ylabel('Annual Deployments (GW/yr)')
-plt.xlim([2000, 2050])
-plt.ylim([0, 400])
-#plt.axhspan(200, 2018, facecolor='0.2', alpha=0.5)
-plt.axvspan(2000, 2018, facecolor='0.9', alpha=0.9)
-
-#plt.axvspan(2019, 2050, facecolor='0.2', alpha=0.5)
-
-
-# In[45]:
-
-
-#fig = plt.figure()
 fig = plt.figure(figsize=(20,10))
 ax1 = plt.subplot(111)
 ax1.yaxis.grid()
 plt.axvspan(2000, 2018, facecolor='0.9', alpha=0.5)
 plt.axvspan(2018, 2050.5, facecolor='yellow', alpha=0.1)
 ax1.bar(r1.scenario['Garvin_2020'].data['year'], r1.scenario['Garvin_2020'].data['new_Installed_Capacity_[MW]']/1000, color='gold', label='IRENA')
-plt.plot(r1.scenario['PV_ICE_default'].data['year'], r1.scenario['PV_ICE_default'].data['new_Installed_Capacity_[MW]']/1000, 'r', label = 'World Baseline')
 plt.legend()
 plt.xlabel('Year')
 plt.ylabel('Annual Deployments (GW/yr)')
@@ -106,96 +91,99 @@ plt.xlim([2000, 2050.5])
 plt.ylim([0, 400])
 
 
-# In[147]:
+# #### Adjusting input parameters to represent the inputs from the IRENA analysis:
+
+# In[7]:
 
 
 r1.scenario['Garvin_2020'].data['mod_Repairing'] = 0
 r1.scenario['Garvin_2020'].data['mod_Repowering'] = 0
 
-r1.scenario['Garvin_2020'].data['mod_degradation'] = 0
-r1.scenario['Garvin_2020'].data['mod_EOL_collection_eff'] = 0  # Everything goes to landfill
-r1.scenario['Garvin_2020'].data['mod_reliability_t50'] = 25
-r1.scenario['Garvin_2020'].data['mod_reliability_t90'] = 38
+r1.scenario['Garvin_2020'].data['mod_degradation'] = 0  # Their calculation does not consider degradation of the fleet.
+
+#We're just calculating total waste so everythign goes to landfill
+r1.scenario['Garvin_2020'].data['mod_EOL_collection_eff'] = 0  
+
+# Setting the shape of the weibull 
+r1.scenario['Garvin_2020'].data['mod_reliability_t50'] = 35
+r1.scenario['Garvin_2020'].data['mod_reliability_t90'] = 45
 # Setting Project Lifetime beyond Failures
 r1.scenario['Garvin_2020'].data['mod_lifetime'] = 40
 
 
-# In[148]:
+# In[8]:
 
 
-r1.scenario['PV_ICE_default'].data.keys()
+print(r1.scenario.keys())
+print("")
+print(r1.scenario['Garvin_2020'].data.keys())
+print("")
+print(r1.scenario['Garvin_2020'].material['glass'].materialdata.keys())
 
 
-# In[149]:
+# In[9]:
 
 
 r1.calculateMassFlow()
 
 
-# In[150]:
+# In[10]:
 
 
-r1.scenario['Garvin_2020'].data.keys()
+print(r1.scenario.keys())
+print("")
+print(r1.scenario['Garvin_2020'].data.keys())
+print("")
+print(r1.scenario['Garvin_2020'].material['glass'].materialdata.keys())
 
 
-# In[151]:
-
-
-r1.scenario['Garvin_2020'].material['glass'].materialdata.keys()
-
-
-# In[152]:
-
-
-r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[-1]/(1E9)
-
-
-# In[153]:
-
-
-r1.scenario['Garvin_2020'].data['year'].iloc[25]
-r1.scenario['Garvin_2020'].data['year'].iloc[35]
-r1.scenario['Garvin_2020'].data['year'].iloc[55]
-
-
-# In[154]:
-
-
-print(r1.scenario['Garvin_2020'].data['year'].iloc[25], r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[25]/(1E12))
-print(r1.scenario['Garvin_2020'].data['year'].iloc[35], r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[35]/(1E12))
-print(r1.scenario['Garvin_2020'].data['year'].iloc[55], r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[55]/(1E12))
-
-
-# ### Remidner NOTES:
-#  mat_Total_Landfilled is in kg, 1 t --> 907.185 kg 
+# ## Irena Conversion from Mass to Energy --> 
+# mat_Total_Landfilled is in g, 1 t --> 907.185 kg 
 #  1 MW --> 76 t conversion for the Mass in PV service.
 
-# In[177]:
+# Querying some of the values for plotting the flags
 
+# In[11]:
 
-x2050 = r1.scenario['Garvin_2020'].data['year'].iloc[55]
-y2050 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[55]*76/1000000
-t2050 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[55]/(1E12)
-
-x2030 = r1.scenario['Garvin_2020'].data['year'].iloc[35]
-y2030 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[35]*76/1000000
-t2030 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[35]/(1E12)
 
 x2020 = r1.scenario['Garvin_2020'].data['year'].iloc[25]
 y2020 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[25]*76/1000000
 t2020 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[25]/(1E12)
 
 
-# In[196]:
+x2030 = r1.scenario['Garvin_2020'].data['year'].iloc[35]
+y2030 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[35]*76/1000000
+t2030 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[35]/(1E12)
+
+x2050 = r1.scenario['Garvin_2020'].data['year'].iloc[55]
+y2050 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[55]*76/1000000
+t2050 = r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]'].iloc[55]/(1E12)
+
+
+# Calculating Cumulative Waste isntead of yearly waste
+# 
+# Using glass for proxy of the module; glass is ~76% of the module's mass [REF]
+# 
+
+# In[12]:
+
+
+cumWaste = r1.scenario['Garvin_2020'].material['glass'].materialdata['mat_Total_Landfilled'].cumsum()
+cumWaste = (cumWaste*100/76)/907185.0  # Converting to tons.
+
+
+# PLOT:
+
+# In[13]:
 
 
 fig = plt.figure(figsize=(10,10))
-plt.semilogy(r1.scenario['Garvin_2020'].data.year,r1.scenario['Garvin_2020'].material['glass'].materialdata['mat_Total_Landfilled']/907.185, label='Mass of PV in service')
-plt.semilogy(r1.scenario['Garvin_2020'].data.year,r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]']*76/1000000, label='Cumulative PV Waste')
+plt.semilogy(r1.scenario['Garvin_2020'].data.year,r1.scenario['Garvin_2020'].data['Installed_Capacity_[W]']*76/1000000, color='C1', label='Mass of PV in service')
+plt.semilogy(r1.scenario['Garvin_2020'].data.year,cumWaste, label='Cumulative PV Waste')
 plt.ylim([1E5, 1E9])
 plt.legend()
 plt.tick_params(axis='y', which='minor')
-plt.xlim([2016,2050])
+plt.xlim([2020,2050])
 plt.grid()
 plt.ylabel('Mass of PV systems (t)')
 plt.xlabel('Years')
@@ -240,5 +228,19 @@ plt.annotate(
 )
 
 plt.show()
+
+
+
+# In[14]:
+
+
+fig = plt.figure(figsize=(10,10))
+plt.semilogy(r1.scenario['Garvin_2020'].data.year,r1.scenario['Garvin_2020'].material['glass'].materialdata['mat_Total_Landfilled'], label='PV Glass Waste per Year')
+plt.legend()
+
+
+# In[ ]:
+
+
 
 
