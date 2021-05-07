b485945b970ec428d0244b4cb4eeefcb591dbf12
Silvana Ayala <silvana.ayala@nrel.gov>
2021-05-07 17:05:52 -0600

    updated REeds Baseline Approach
    ReEDS input is annually reported, not biannually, so teh previous inverse cumsum was wrong. Futhermore, data was cumulative capacity; new changes reflect new installs only (use ReEDS input file v3).
    
    ALSO: NREL ENergy Futures also updated to be new installs instaed of cumulative installs, and repeating hte odd year the value of the next even year.
    
    First year on ReEDS is cumulative value of all previous years. Instead of using cumulative value, for EF the PV ICE module baseline was added from 1995 to 2019 with good agreemenet on new installed capacity.

--- docs/tutorials/ReEDS Simulations/(development) ReEDS Scenarios Baseline Creation.py
+++ docs/tutorials/ReEDS Simulations/(development) ReEDS Scenarios Baseline Creation.py
@@ -41,7 +41,7 @@ print ("Your simulation will be stored in %s" % testfolder)
 # In[3]:
 
 
-reedsFile = str(Path().resolve().parent.parent.parent.parent / 'December Core Scenarios ReEDS Outputs Solar Futures v2a.xlsx')
+reedsFile = str(Path().resolve().parent.parent.parent.parent / 'December Core Scenarios ReEDS Outputs Solar Futures v3a.xlsx')
 print ("Input file is stored in %s" % reedsFile)
 
 
@@ -49,7 +49,7 @@ print ("Input file is stored in %s" % reedsFile)
 
 
 REEDSInput = pd.read_excel(reedsFile,
-                        sheet_name="UPV Capacity (GW)")
+                        sheet_name="new installs PV")
                         #index_col=[0,2,3]) #this casts scenario, PCA and State as levels
 
 
@@ -84,6 +84,24 @@ baseline.head()
 
 # #### For each Scenario and for each PCA, combine with baseline and save as input file
 
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
 # In[7]:
 
 
@@ -98,20 +116,12 @@ for ii in range (len(rawdf.unstack(level=1))):
     A.name = 'new_Installed_Capacity_[MW]'
     A = pd.DataFrame(A)
     A.index=pd.PeriodIndex(A.index, freq='A')
-    A = A.resample('Y').asfreq()
-    A = A['new_Installed_Capacity_[MW]'].fillna(0).groupby(A['new_Installed_Capacity_[MW]'].notna().cumsum()).transform('mean')    
     A = pd.DataFrame(A)
-    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * df_marketshares['Si']
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 0.85
     A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
     # Add other columns
     A = pd.concat([A, baseline.reindex(A.index)], axis=1)
-    A.loc['2050']['new_Installed_Capacity_[MW]'] = A.loc['2050']['new_Installed_Capacity_[MW]']/2   # Dividing last value by 2
-    new_row = A[-1:]
-    new_row.index = new_row.index.shift(periods=1)   #Shifting so new row is 2051
-    A = pd.concat([A,pd.concat([new_row])])              
-    A.index = A.index.shift(periods=-1)          # Shifting back so it goes from 2009-2050
-
-    
+   
     header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"    "mod_Repowering,mod_Repairing\n"    "year,MW,%,years,years,%,years,%,%,%,%,%\n"
 
     with open(filetitle, 'w', newline='') as ict:
@@ -154,7 +164,7 @@ df.head()
 # In[10]:
 
 
-for ii in range (len(df.unstack(level=2))):
+for ii in range (len(df.unstack(level=2))):   
     STATE = df.unstack(level=2).iloc[ii].name[1]
     SCEN = df.unstack(level=2).iloc[ii].name[0]
     SCEN=SCEN.replace('+', '_')
@@ -165,19 +175,12 @@ for ii in range (len(df.unstack(level=2))):
     A.name = 'new_Installed_Capacity_[MW]'
     A = pd.DataFrame(A)
     A.index=pd.PeriodIndex(A.index, freq='A')
-    A = A.resample('Y').asfreq()
-    A = A['new_Installed_Capacity_[MW]'].fillna(0).groupby(A['new_Installed_Capacity_[MW]'].notna().cumsum()).transform('mean')    
     A = pd.DataFrame(A)
-    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * df_marketshares['Si']
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 0.85 # marketshares['Si']
     A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
     # Add other columns
     A = pd.concat([A, baseline.reindex(A.index)], axis=1)
-    A.loc['2050']['new_Installed_Capacity_[MW]'] = A.loc['2050']['new_Installed_Capacity_[MW]']/2   # Dividing last value by 2
-    new_row = A[-1:]
-    new_row.index = new_row.index.shift(periods=1)   #Shifting so new row is 2051
-    A = pd.concat([A,pd.concat([new_row])])              
-    A.index = A.index.shift(periods=-1)          # Shifting back so it goes from 2009-2050
-
+    
     
     header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"    "mod_Repowering,mod_Repairing\n"    "year,MW,%,years,years,%,years,%,%,%,%,%\n"
 
@@ -192,72 +195,11 @@ for ii in range (len(df.unstack(level=2))):
         A.to_csv(ict, header=False)
 
 
-# ## Method with PIVOT tables for STATES (REDUNDANT)
-
-# In[11]:
-
-
-"""
-df = pd.read_excel(reedsFile,
-                        sheet_name="UPV Capacity (GW)")
-                        #index_col=[0,2,3]) #this casts scenario, PCA and State as levels
-df.drop(columns=['Tech'], inplace=True)
-
-#foo = df.pivot_table(index=['Scenario','Year'],columns='State',aggfunc=sum)
-foo = df.pivot_table(index=['Scenario','State'],columns='Year',aggfunc=sum)
-
-foo = pd.DataFrame(foo)
-foo.head()
-
-
-for ii in range (len(foo)):
-    STATE = foo.index[ii][1]
-    SCEN = foo.index[ii][0]
-    SCEN=SCEN.replace('+', '_')
-    filetitle = SCEN+'_'+STATE +'.csv'
-    filetitle = os.path.join(testfolder, 'STATEs', filetitle)
-    A = pd.DataFrame(foo.iloc[ii])
-    #A = A.unstack(level=0).iloc[ii]
-    A = A.droplevel(level=0)
-    A.columns = [' '.join(col).strip() for col in A.columns.values]
-    A.rename(columns = {list(A)[0]: 'new_Installed_Capacity_[MW]'}, inplace = True)
-    A.index=pd.PeriodIndex(A.index, freq='A')
-    A = A.resample('Y').asfreq()
-    A = A['new_Installed_Capacity_[MW]'].fillna(0).groupby(A['new_Installed_Capacity_[MW]'].notna().cumsum()).transform('mean')    
-    A = pd.DataFrame(A)
-    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
-    # Add other columns
-    A = pd.concat([A, baseline.reindex(A.index)], axis=1)
-    A.loc['2050']['new_Installed_Capacity_[MW]'] = A.loc['2050']['new_Installed_Capacity_[MW]']/2   # Dividing last value by 2
-    new_row = A[-1:]
-    new_row.index = new_row.index.shift(periods=1)   #Shifting so new row is 2051
-    A = pd.concat([A,pd.concat([new_row])])              
-    A.index = A.index.shift(periods=-1)          # Shifting back so it goes from 2009-2050
-
-    header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"\
-    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"\
-    "mod_Repowering,mod_Repairing\n"\
-    "year,MW,%,years,years,%,years,%,%,%,%,%\n"
-
-    with open(filetitle, 'w', newline='') as ict:
-    # Write the header lines, including the index variable for
-    # the last one if you're letting Pandas produce that for you.
-    # (see above).
-        for line in header:
-            ict.write(line)
-
-        #    savedata.to_csv(ict, index=False)
-        A.to_csv(ict, header=False)
-        
-"""
-pass
-
-
 # # Saving US Baseline
 
 # ### Create a copy of the REEDS Input and modify structure for PCA focus
 
-# In[12]:
+# In[11]:
 
 
 rawdf = REEDSInput.copy()
@@ -267,7 +209,7 @@ rawdf.set_index(['Scenario','Year'], inplace=True)
 rawdf.head(21)
 
 
-# In[13]:
+# In[12]:
 
 
 df = rawdf.groupby(['Scenario','Year'])['Capacity (GW)'].sum(axis=0)
@@ -275,7 +217,7 @@ df = rawdf.groupby(['Scenario','Year'])['Capacity (GW)'].sum(axis=0)
 
 # ### Loading Module Baseline. Will be used later to populate all the columsn other than 'new_Installed_Capacity_[MW]' which will be supplied by the REEDS model
 
-# In[14]:
+# In[13]:
 
 
 import PV_ICE
@@ -290,7 +232,7 @@ baseline.head()
 
 # ### For each Scenario, combine with baseline and save as input file¶
 
-# In[15]:
+# In[14]:
 
 
 for ii in range (len(df.unstack(level=1))):
@@ -303,20 +245,12 @@ for ii in range (len(df.unstack(level=1))):
     A.name = 'new_Installed_Capacity_[MW]'
     A = pd.DataFrame(A)
     A.index=pd.PeriodIndex(A.index, freq='A')
-    A = A.resample('Y').asfreq()
-    A = A['new_Installed_Capacity_[MW]'].fillna(0).groupby(A['new_Installed_Capacity_[MW]'].notna().cumsum()).transform('mean')    
     A = pd.DataFrame(A)
-    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * df_marketshares['Si']
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 0.85 # marketshares['Si']
     A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
     # Add other columns
     A = pd.concat([A, baseline.reindex(A.index)], axis=1)
-    A.loc['2050']['new_Installed_Capacity_[MW]'] = A.loc['2050']['new_Installed_Capacity_[MW]']/2   # Dividing last value by 2
-    new_row = A[-1:]
-    new_row.index = new_row.index.shift(periods=1)   #Shifting so new row is 2051
-    A = pd.concat([A,pd.concat([new_row])])              
-    A.index = A.index.shift(periods=-1)          # Shifting back so it goes from 2009-2050
-
-    
+   
     header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"    "mod_Repowering,mod_Repairing\n"    "year,MW,%,years,years,%,years,%,%,%,%,%\n"
 
     with open(filetitle, 'w', newline='') as ict:
