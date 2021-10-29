aa32838c3f898049bba6bcf2cdb069f1b724f56b
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2021-10-29 16:52:08 -0600

    Created updated analysis for Lifetime vs Recycling Paper
    completed the simulation setup to the point of running the mass flow calculation the first time (not install compensation).

--- docs/tutorials/13 - LifetimeVsRecycling Analysis.py
+++ docs/tutorials/13 - LifetimeVsRecycling Analysis.py
@@ -32,13 +32,15 @@ from pathlib import Path
 PV_ICE.__version__
 
 
-# In[21]:
+# In[2]:
 
 
 testfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'TEMP' / 'SFs_LifeVSRecycle')
 inputfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'TEMP')
+materialsfolder = str(Path().resolve().parent.parent /'PV_ICE' / 'baselines')
 print ("Your simulation will be stored in %s" % testfolder)
 print("Your simulation input data will be written/read from %s" % inputfolder)
+print("Material files will be from %s" % materialsfolder)
 
 
 # In[3]:
@@ -50,21 +52,49 @@ if not os.path.exists(testfolder):
 
 # # File Preparation
 
-# In[16]:
+# First, we load the Module Baseline. Will be used later to populate all the columns other than 'new_Installed_Capacity_[MW]' which will be supplied by the REEDS model. Unlike the SF simulations, this analysis will use PV ICE developed baselines.
+
+# NOTE: this section of code should only need to be run once to populate data, and again anytime the ReeDS file is updated.
+
+# In[4]:
+
+
+rtest = PV_ICE.Simulation(name='Sim1', path=inputfolder)
+rtest.createScenario(name='test', file=r'..\baselines\baseline_modules_US.csv')
+baseline = rtest.scenario['test'].data
+baseline = baseline.drop(columns=['new_Installed_Capacity_[MW]'])
+baseline.set_index('year', inplace=True)
+baseline.index = pd.PeriodIndex(baseline.index, freq='A')  # A -- Annual
+#baseline.head(5)
+
+
+# Drop 1995 through 2009 because SF projections begin in 2010. Technically this neglects ~1.5 GW of installs from 1995 through 2009.
+
+# In[5]:
+
+
+baseline.drop(baseline.loc['1995':'2009'].index, inplace=True)
+
+
+# Now we load the ReEDS simulation output, i.e. the Solar Futures projections with PCA regions, States, and Scenarios. Note that this is stored outside of the PV ICE folder and therefore not publicly available on github
+
+# In[6]:
 
 
 reedsFile = str(Path().resolve().parent.parent.parent / 'December Core Scenarios ReEDS Outputs Solar Futures v3a.xlsx')
 print ("Input file is stored in %s" % reedsFile)
 
 
-# In[18]:
+# In[7]:
 
 
 REEDSInput = pd.read_excel(reedsFile, sheet_name="new installs PV")
 #NEED TO CHECK THAT AC to DC has been taken care of in excel file (there is a multiplier, is set to 1)
 
 
-# In[19]:
+# First create a copy which groups the data by PCA region
+
+# In[8]:
 
 
 rawdf = REEDSInput.copy()
@@ -73,137 +103,160 @@ rawdf.drop(columns=['Tech'], inplace=True) #tech=pvtotal from "new installs PV s
 rawdf.set_index(['Scenario','Year','PCA'], inplace=True)
 
 
-# Loading Module Baseline. Will be used later to populate all the columns other than 'new_Installed_Capacity_[MW]' which will be supplied by the REEDS model. Unlike the SF simulations, this analysis will use PV ICE developed baselines.
+# For each Scenario and for each PCA, combine with baseline and save as input file. This will be in a folder PCAs under the simulation folder in TEMP
 
-# In[23]:
+# In[9]:
 
 
-rtest = PV_ICE.Simulation(name='Sim1', path=inputfolder)
-rtest.createScenario(name='test', file=r'..\baselines\baseline_modules_US.csv') #likely to cause year issue
-baseline = rtest.scenario['test'].data
-baseline = baseline.drop(columns=['new_Installed_Capacity_[MW]'])
-baseline.set_index('year', inplace=True)
-baseline.index = pd.PeriodIndex(baseline.index, freq='A')  # A -- Annual
-baseline.head(5)
+for ii in range (len(rawdf.unstack(level=1))):
+    PCA = rawdf.unstack(level=1).iloc[ii].name[1]
+    SCEN = rawdf.unstack(level=1).iloc[ii].name[0]
+    SCEN=SCEN.replace('+', '_')
+    filetitle = SCEN+'_'+PCA +'.csv'
+    subtestfolder = os.path.join(testfolder, 'PCAs')
+    if not os.path.exists(subtestfolder):
+        os.makedirs(subtestfolder)
+    filetitle = os.path.join(subtestfolder, filetitle)
+    A = rawdf.unstack(level=1).iloc[ii]
+    A = A.droplevel(level=0)
+    A.name = 'new_Installed_Capacity_[MW]'
+    A = pd.DataFrame(A)
+    A.index=pd.PeriodIndex(A.index, freq='A')
+    A = pd.DataFrame(A)
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 0.85
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
+    # Add other columns
+    A = pd.concat([A, baseline.reindex(A.index)], axis=1)
+   
+    header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"    "mod_Repair,mod_MerchantTail,mod_Reuse\n"    "year,MW,%,years,years,%,years,%,%,%,%,%,%\n"
 
+    with open(filetitle, 'w', newline='') as ict:
+    # Write the header lines, including the index variable for
+    # the last one if you're letting Pandas produce that for you.
+    # (see above).
+        for line in header:
+            ict.write(line)
 
-# In[ ]:
+        #    savedata.to_csv(ict, index=False)
+        A.to_csv(ict, header=False)
 
 
+# For each Scenario and each State, combine with baseline file and save as input file. This will be in a folder States under the simulation folder in TEMP
 
+# In[10]:
 
 
-# In[ ]:
+rawdf = REEDSInput.copy()
+#rawdf.drop(columns=['State'], inplace=True)
+rawdf.drop(columns=['Tech'], inplace=True)
+rawdf.set_index(['Scenario','Year','PCA', 'State'], inplace=True)
 
 
+# In[11]:
 
 
+df = rawdf.groupby(['Scenario','State', 'Year'])['Capacity (GW)'].sum()
+df = pd.DataFrame(df)
 
-# In[ ]:
 
+# In[12]:
 
 
+for ii in range (len(df.unstack(level=2))):   
+    STATE = df.unstack(level=2).iloc[ii].name[1]
+    SCEN = df.unstack(level=2).iloc[ii].name[0]
+    SCEN=SCEN.replace('+', '_')
+    filetitle = SCEN+'_'+STATE +'.csv'
+    
+    subtestfolder = os.path.join(testfolder, 'States')
+    if not os.path.exists(subtestfolder):
+        os.makedirs(subtestfolder)
+    filetitle = os.path.join(subtestfolder, filetitle)
 
+    A = df.unstack(level=2).iloc[ii]
+    A = A.droplevel(level=0)
+    A.name = 'new_Installed_Capacity_[MW]'
+    A = pd.DataFrame(A)
+    A.index=pd.PeriodIndex(A.index, freq='A')
+    A = pd.DataFrame(A)
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 0.85 # marketshares['Si']
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
+    # Add other columns
+    A = pd.concat([A, baseline.reindex(A.index)], axis=1)
+    
+    
+    header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"    "mod_Repair,mod_MerchantTail,mod_Reuse\n"    "year,MW,%,years,years,%,years,%,%,%,%,%,%\n"
 
-# In[ ]:
+    with open(filetitle, 'w', newline='') as ict:
+    # Write the header lines, including the index variable for
+    # the last one if you're letting Pandas produce that for you.
+    # (see above).
+        for line in header:
+            ict.write(line)
 
+        #    savedata.to_csv(ict, index=False)
+        A.to_csv(ict, header=False)
 
 
+# Finally, make an overall US baseline which ignores PCA regions and states. This is useful for speeding the simulation.
 
+# In[13]:
 
-# In[ ]:
 
+rawdf = REEDSInput.copy()
+#rawdf.drop(columns=['State'], inplace=True)
+rawdf.drop(columns=['Tech'], inplace=True)
+rawdf.set_index(['Scenario','Year'], inplace=True)
 
 
+# In[14]:
 
 
-# In[ ]:
+df = rawdf.groupby(['Scenario','Year'])['Capacity (GW)'].sum()
 
 
+# In[15]:
 
 
+for ii in range (len(df.unstack(level=1))):
+    SCEN = df.unstack(level=1).index[ii]
+    SCEN=SCEN.replace('+', '_')
+    filetitle = SCEN+'.csv'
+    
+    subtestfolder = os.path.join(testfolder, 'USA')
+    if not os.path.exists(subtestfolder):
+        os.makedirs(subtestfolder)
+    filetitle = os.path.join(subtestfolder, filetitle)
+    
+    A = df.unstack(level=1).iloc[ii]
 
-# In[ ]:
+    A.name = 'new_Installed_Capacity_[MW]'
+    A = pd.DataFrame(A)
+    A.index=pd.PeriodIndex(A.index, freq='A')
+    A = pd.DataFrame(A)
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 0.85 # marketshares['Si']
+    A['new_Installed_Capacity_[MW]'] = A['new_Installed_Capacity_[MW]'] * 1000   # ReEDS file is in GW.
+    # Add other columns
+    A = pd.concat([A, baseline.reindex(A.index)], axis=1)
+   
+    header = "year,new_Installed_Capacity_[MW],mod_eff,mod_reliability_t50,mod_reliability_t90,"    "mod_degradation,mod_lifetime,mod_MFG_eff,mod_EOL_collection_eff,mod_EOL_collected_recycled,"    "mod_Repair,mod_MerchantTail,mod_Reuse\n"    "year,MW,%,years,years,%,years,%,%,%,%,%,%\n"
 
+    with open(filetitle, 'w', newline='') as ict:
+    # Write the header lines, including the index variable for
+    # the last one if you're letting Pandas produce that for you.
+    # (see above).
+        for line in header:
+            ict.write(line)
 
+        #    savedata.to_csv(ict, index=False)
+        A.to_csv(ict, header=False)
 
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# # Analysis
 
 # Collect all the scenario names and downselect to the scenario(s) of interest. In this case, we are only concerned with the highest capacity and deployment rate, Decarbonization + Electrification (Decarb+E)
 
-# In[4]:
+# In[3]:
 
 
 scenarios = ['Reference.Mod',
@@ -220,7 +273,7 @@ SFscenarios = ['95-by-35_Elec.Adv_DR'] #Decarb+E
 SFscenarios
 
 
-# In[7]:
+# In[4]:
 
 
 #add materials for simulation
@@ -229,30 +282,191 @@ MATERIALS = ['glass','aluminium_frames','silicon','silver', 'copper', 'encapsula
 
 # Set up the PV ICE simulation with scenario and materials
 
-# In[10]:
+# In[5]:
 
 
-MATERIALBASELINE = r'..\..\baselines\baseline_material_'+MATERIALS[0]+'.csv'
-MATERIALBASELINE
+r1 = PV_ICE.Simulation(name='SF-LvR', path=testfolder) #create simulation r1
+
+for scen in range(len(SFscenarios)):
+    modulefile = SFscenarios[scen]+'.csv' #pick the scenario csv
+    modulefile = os.path.join(testfolder, 'USA', modulefile) #point at the file path for the whole US
+    r1.createScenario(name='Decarb+E_PVICE_defaults', file=modulefile) #change name=SFscenarios[scen] if multiple scenarios
+    for mat in range(len(MATERIALS)):
+        materialfile = 'baseline_material_'+ MATERIALS[mat] +'.csv' #name the material file
+        materialfile = os.path.join(materialsfolder,materialfile) #point at the material file in the baselines folder
+        r1.scenario['Decarb+E_PVICE_defaults'].addMaterial(MATERIALS[mat], file=materialfile)#change SFscenarios[scen] if multiple scen
+        #remove 1995-2009 from each material
+        matdf = r1.scenario['Decarb+E_PVICE_defaults'].material[MATERIALS[mat]].materialdata #pull out the df
+        matdf.set_index('year', inplace=True) #assign the year index to the df
+        matdf.drop(matdf.loc['1995':'2009'].index, inplace=True) #drop the early years
+        r1.scenario['Decarb+E_PVICE_defaults'].material[MATERIALS[mat]].materialdata = matdf #reassign the material data to the simulation
 
 
-# In[14]:
+# In[8]:
 
 
-modulefile = SFscenarios[0]+'.csv'
-os.path.join(inputfolder, 'USA', modulefile) #need USA bc that's where ReEDS Scenarios Baseline Creation stores its output)
+#r1.scenario['Decarb+E_PVICE_defaults'].material['glass'].keys()
+#r1.scenario['Decarb+E_PVICE_defaults'].material['copper'].materialdata.index
+
+
+# Run the simulation
+
+# In[7]:
+
+
+r1.calculateMassFlow()
+
+
+# In[9]:
+
+
+r1.plotScenariosComparison('Installed_Capacity_[W]')
+
+
+# ## Lifetime and Recycling Scenario Creation
+
+# The range of potential future technology directions for PV will be explored in terms of module lifetime and EoL recycling rates. Currently technology is ~32 year module with a 6% EoL recycling rate (15% collection, 40% modules sent to recycling). Lifetimes could improve, with 50 years targeted by DOE SETO. And or recycling rates could improve, as modeled by CdTe management from First Solar or perovskite technology. This analysis will explore on a mass flow basis, which of these two circular economy levers is most important research priority for achieving the energy transition while minimizing waste and material extraction.
+# 
+# We will explore from a 15 year module lifetime to a 50 year module lifetime, and from 0% recycled to 100% recycled.
+
+# ### Create lifetime and recycling ranges
+
+# In[30]:
+
+
+Lifetime_Range = pd.concat([pd.Series(range(15,30,3)),pd.Series(range(30,51,2))]) #this absolute lifetime values
+Lifetime_Range.reset_index(inplace=True, drop=True)
+Lifetime_Diff30 = Lifetime_Range-30
+Degradation_Range = pd.Series([1.470, 1.220, 1.050, 0.920, 0.820, 0.740, 0.690, 0.650, 0.615, 0.582, 0.555, 0.525, 0.505, 0.480, 0.460, 0.445])
+Recycling_Range = pd.Series(range(0,105,5)) # this is absolute recycling values from 0-100
+#print(Lifetime_Range)
+#print(Degradation_Range)
+#print(Recycling_Range)
+
+
+# In[47]:
+
+
+#list of material recycling variables
+RecyclingPaths = ['mat_MFG_scrap_Recycled', 'mat_MFG_scrap_Recycled_into_HQ', 'mat_MFG_scrap_Recycled_into_HQ_Reused4MFG', 'mat_EOL_collected_Recycled', 'mat_EOL_Recycled_into_HQ', 'mat_EOL_RecycledHQ_Reused4MFG']
+RecyclingYields = ['mat_MFG_scrap_Recycling_eff', 'mat_EOL_Recycling_eff']
+
+
+# Now some magic to automatically generate T50 and T90 values for each lifetime
+
+# In[32]:
+
+
+#create linear regression for mod_reliability_t50 & mod_reliability_t90 vs. mod_lifetime 
+#to estimate t50 and t90 values to input for improved lifetime scenario
+reliability_baselines = pd.DataFrame()
+reliability_baselines['mod_lifetime'] = r1.scenario['Decarb+E_PVICE_defaults'].data['mod_lifetime']
+reliability_baselines['mod_reliability_t50'] = r1.scenario['Decarb+E_PVICE_defaults'].data['mod_reliability_t50']
+reliability_baselines['mod_reliability_t90'] = r1.scenario['Decarb+E_PVICE_defaults'].data['mod_reliability_t90']
+
+
+# In[33]:
+
+
+X_lifetime = reliability_baselines.iloc[:, 0].values.reshape(-1, 1)  # values converts it into a numpy array
+Y1_t50 = reliability_baselines.iloc[:, 1].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
+Y2_t90 = reliability_baselines.iloc[:, 2].values.reshape(-1, 1)
+range_lifetimes = np.array(Lifetime_Range).reshape(-1,1)
+
+from sklearn.linear_model import LinearRegression
+from itertools import chain
+
+linear_regressor_Y1 = LinearRegression()
+linear_regressor_Y1.fit(X_lifetime, Y1_t50)  # perform linear regression
+t50_list = linear_regressor_Y1.predict(range_lifetimes).tolist()  # make predictions based on improved lifetime values
+t50_list = list(chain(*t50_list)) #unnest list
+t50_range_simple = pd.Series([ '%.2f' % elem for elem in t50_list ])
+
+linear_regressor_Y2 = LinearRegression() 
+linear_regressor_Y2.fit(X_lifetime, Y2_t90)
+t90_list = linear_regressor_Y2.predict(range_lifetimes).tolist()
+t90_list = list(chain(*t90_list)) #unnest list
+t90_range_simple = pd.Series([ '%.2f' % elem for elem in t90_list ])
+
+
+# In[34]:
+
+
+#create a tidy dataframe summarizing all the lifetime, degradation, reliability values
+lifetime_range_df = pd.concat([Lifetime_Range, Degradation_Range, t50_range_simple, t90_range_simple], axis=1)
+lifetime_range_df.columns = 'mod_lifetime', 'mod_degradation', 't50', 't90'
+print(lifetime_range_df)
+
+
+# In[35]:
+
+
+#drop some of the higher lifetime values due to small value add and graphing
+unnecessary = [48,46,42,38,34]
+lifetime_range_df = lifetime_range_df[lifetime_range_df.mod_lifetime.isin(unnecessary)==False]
+print(lifetime_range_df)
+
+
+# Now with the lifetime and recycling ranges defined, create a scenario for each combination
+# 
+# Notes:
+# - recycling values are set to closed loop, with XX% material recycling yields assuming 100% collection of modules and materials
+# - 
+
+# In[53]:
+
+
+#these scenarios are being added onto the Decarb+E_PVICE_Default scenario
+#All combinations of recycling and lifetime
+for life in range(0,len(Lifetime_Range)): #loop over lifetimes
+    for recycle in range (0,len(Recycling_Range)): #loop over recycling rates
+        scenname = str(Lifetime_Range[life])+'years & '+ str(Recycling_Range[recycle])+'% Recycled' #name the scenario
+        r1.createScenario(name=scenname,file=modulefile) #create the scenario with name
+        #MODIFY LIFETIME PARAMETERS HERE
+        r1.scenario[scenname].data['mod_lifetime'] = Lifetime_Range[life]
+        r1.scenario[scenname].data['mod_reliability_t50'] = float(t50_range_simple[life])
+        r1.scenario[scenname].data['mod_reliability_t90'] = float(t90_range_simple[life])
+        r1.scenario[scenname].data['mod_degradation'] = Degradation_Range[life]
+        #MODIFY Module RECYCLING PARAMATERS HERE
+        #the assumption is all modules and materials are collected and sent to recycling, only material recycling yields vary
+        r1.scenario[scenname].data['mod_EOL_collected_recycled'] = 100.0
+        r1.scenario[scenname].data['mod_EOL_collection_eff'] =100.0
+        
+        #Add Materials to scenario
+        for mat in range (0,len(MATERIALS)):
+            materialfile = 'baseline_material_'+ MATERIALS[mat] +'.csv' #name the material file
+            materialfile = os.path.join(materialsfolder,materialfile) #point at the material file in the baselines folder
+            r1.scenario[scenname].addMaterial(MATERIALS[mat], file=materialfile)
+            
+            #remove 1995-2009 from each material
+            matdf = r1.scenario[scenname].material[MATERIALS[mat]].materialdata #pull out the df
+            matdf.set_index('year', inplace=True) #assign the year index to the df
+            matdf.drop(matdf.loc['1995':'2009'].index, inplace=True) #drop the early years
+            r1.scenario[scenname].material[MATERIALS[mat]].materialdata = matdf #reassign the material data to the simulation
+            
+            #Modify Material recycling parameters for 2021 and forward
+            for var in range(0,len(RecyclingPaths)):
+                r1.scenario[scenname].material[MATERIALS[mat]].materialdata[RecyclingPaths[var]].loc[2021:] = 100.0
+            for ylds in range(0,len(RecyclingYields)):
+                r1.scenario[scenname].material[MATERIALS[mat]].materialdata[RecyclingYields[ylds]].loc[2021:] = Recycling_Range[recycle]
+            
+            
+
+
+# In[60]:
+
+
+print(len(r1.scenario.keys()))
 
 
 # In[ ]:
 
 
-r1 = PV_ICE.Simulation(name='SF-LvR', path=testfolder)
+r1.calculateMassFlow()
 
-for scen in range(len(SFscenarios)):
-    modulefile = SFscenarios[scen]+'.csv'
-    modulefile = os.path.join(inputfolder, 'USA', modulefile)
-    r1.createScenario(name='Decarb+E_PVICE_defaults', file=modulefile) #change name=SFscenarios[scen] if multiple scenarios
-    for mat in range(len(MATERIALS)):
-        MATERIALBASELINE = r'..\..\baselines\baseline_material_'+MATERIALS[mat]+'.csv'
-        r1.scenario[SFscenarios[scen]].addMaterial(MATERIALS[mat], file=MATERIALBASELINE)
+
+# In[ ]:
+
+
+
 
