baf864fa80d91c3112a76d100e95bbcbc2c21da4
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2022-07-26 14:24:38 -0600

    updating baselines part 2
    continued updating numbers in supporting material. reran module efficiency and updated bifacial numbers baselines. need to finish updating supporting material, then run journals, then updating baselines.

--- docs/tutorials/baseline development documentation/(baseline development) Module Efficiency.py
+++ docs/tutorials/baseline development documentation/(baseline development) Module Efficiency.py
@@ -63,7 +63,7 @@ def power_law(x, a, b):
     return a*np.power(x, b)
 
 
-# In[19]:
+# In[7]:
 
 
 #generae a dataset for the area in between
@@ -74,7 +74,7 @@ print(y_dummy[30])
 plt.plot(y_dummy)
 
 
-# In[18]:
+# In[8]:
 
 
 #create a dataframe of the projection
@@ -103,7 +103,7 @@ plt.ylabel('Efficiency (%)')
 plt.rcParams.update({'font.size': 22})
 plt.rcParams['figure.figsize'] = (12, 8)
 
-plt.axvspan(2020, 2050.5, facecolor='gray', alpha=0.1)
+plt.axvspan(2021, 2050.5, facecolor='gray', alpha=0.1)
 plt.plot(mod_eff_raw, marker='o', label='Raw Data')
 plt.plot(mod_eff, '--k', label='PV ICE Baseline')
 plt.title('Average Module Efficiency [%]')
@@ -112,14 +112,30 @@ plt.legend()
 plt.xlim([1974, 2050.5])
 
 
-# In[ ]:
+# # Bifaciality Factor
+# 
+# With the advent of bifacial cells and modules with transparent backsheets/glass, there is a bonus energy produced from the slightly less efficient backside cell. This section of the journal records the increasing bifaciality factor, which is how this extra generation is currently tracked (2021).
+
+# Current procedure is to update the historical and projected factors in the baseline file, then run it through here to interpolate, and replace the file.
+
+# In[11]:
 
 
+cwd = os.getcwd() #grabs current working directory
+bifi_raw = pd.read_csv(cwd+"/../../../PV_ICE/baselines/baseline_bifaciality_factor.csv", 
+                          index_col='Year')
 
 
-
-# In[ ]:
-
+# In[14]:
 
 
+bifi = bifi_raw.interpolate()
+plt.plot(bifi)
+plt.ylim(0,1.0)
+
+
+# In[15]:
+
+
+bifi.to_csv(cwd+'/../../../PV_ICE/baselines/baseline_bifaciality_factor.csv', index=True)
 
