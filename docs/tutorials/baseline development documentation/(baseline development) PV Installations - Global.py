501f2f7d9bbf6074dba2a9846affffaaa9f8fb6e
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2022-10-11 13:19:45 -0600

    Created cSi weighted installs for regional and countrywise global installs
    new csvs, callable for installation historical

--- docs/tutorials/baseline development documentation/(baseline development) PV Installations - Global.py
+++ docs/tutorials/baseline development documentation/(baseline development) PV Installations - Global.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# # Calculations for PV Global installs, c-Si Marketshare
+
+# This journal documents the manipulation of PV installation data for Global installs. This covers selection of data, and weighting by marketshare.
+
+# In[1]:
 
 
 import numpy as np
@@ -12,19 +16,21 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 28})
 plt.rcParams['figure.figsize'] = (30, 15)
 
+cwd = os.getcwd() #grabs current working directory
 
-# This journal documents the manipulation of PV installation data for Global installs. This covers selection of data, and weighting by marketshare.
+supportMatfolder = str(Path().resolve().parent.parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial')
 
-# In[9]:
+
+# In[2]:
 
 
 cwd = os.getcwd() #grabs current working directory
-df_installs_raw = pd.read_csv(str(Path().resolve().parent.parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial'/'PVInstalls_World_AllSources.csv'), index_col='Year')
+df_installs_raw = pd.read_csv(os.path.join(supportMatfolder,'PVInstalls_World_AllSources.csv'), index_col='Year')
 sources = df_installs_raw.columns
 print(len(sources))
 
 
-# In[10]:
+# In[3]:
 
 
 plt.plot(df_installs_raw.index,df_installs_raw[sources[0]],lw=4,marker='*',label=sources[0])
@@ -42,7 +48,7 @@ plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left")
 
 # Based on the above graph, we will utilize Goetzberger data through 2000, then IRENA online query tool after 2000.
 
-# In[35]:
+# In[ ]:
 
 
 #Before 2000 = Goetz
@@ -59,7 +65,7 @@ installs_recent_IRENA.columns = ['installed_pv_MW']
 
 # ### Collect the installation data together into a single df
 
-# In[36]:
+# In[ ]:
 
 
 installs = pd.concat([installs_old_Goetz,installs_recent_IRENA])
@@ -72,42 +78,53 @@ plt.title('Installations of PV Globally (MW) since 1995')
 
 # In addition to compiling a single installation record for 1995 through the present, this data is total cumulative, but the tool it currently considering crystalline silicon technology only (i.e. mono and multi, but not ribbon or amorphous).
 
-# In[38]:
+# In[11]:
 
 
 cwd = os.getcwd() #grabs current working directory
-df_raw_mrktshr_siVtf = pd.read_csv(cwd+"/../../PV_ICE/baselines/SupportingMaterial/MarketShare_global_c-SiVSthinfilm.csv", index_col='Year')
+df_raw_mrktshr_siVtf = pd.read_csv(os.path.join(supportMatfolder,'MarketShare_global_c-SiVSthinfilm.csv'), index_col='Year')
 refs = df_raw_mrktshr_siVtf.columns
 print(len(refs))
 
 
-# In[41]:
+# In[12]:
 
 
+plt.rcParams.update({'font.size': 14})
+plt.rcParams['figure.figsize'] = (12, 6)
 plt.plot(df_raw_mrktshr_siVtf.index,df_raw_mrktshr_siVtf[refs[0]],marker='o',label=refs[0])
 plt.plot(df_raw_mrktshr_siVtf.index,df_raw_mrktshr_siVtf[refs[1]],marker='o',label=refs[1])
 plt.plot(df_raw_mrktshr_siVtf.index,df_raw_mrktshr_siVtf[refs[2]],marker='o',label=refs[2])
 plt.plot(df_raw_mrktshr_siVtf.index,df_raw_mrktshr_siVtf[refs[3]],marker='o',label=refs[3])
 plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left")
-plt.ylim(0,100)
+plt.ylim(50,100)
 
 
 # The 2020 Fraunhofer and 2014 Fraunhofer appear to agree reasonably closely, and Mints agrees closely for the amount of time there is overlap. The unknown sourced wikipedia figure doesn't agree until 2010, but given the unknown source, it will be discarded. We will use the Fraunhofer ISE 2020 market share data for the entire time period.
 
-# In[43]:
+# In[13]:
 
 
-df_mrktshr_global =  pd.DataFrame(df_raw_mrktshr_siVtf[refs[2]])
-mrktshr_global = df_mrktshr_global.loc[(df_mrktshr_global.index>=1995) & (df_mrktshr_global.index<2020)]
+refs
+
+
+# In[14]:
+
+
+df_mrktshr_global =  pd.DataFrame(df_raw_mrktshr_siVtf[refs[2]]) #select Fraunhofer for historical 1995-2020
+mrktshr_global = df_mrktshr_global.loc[(df_mrktshr_global.index>=1995)]
+# use PMints for most recent data
 mrktshr_global.columns = ['Global_MarketShare']
 #print(mrktshr_global)
 
 
-# In[45]:
+# In[15]:
 
 
 #convert to decimal
 mrktshr_global_pct = mrktshr_global/100
+
+plt.rcParams['figure.figsize'] = (12, 6)
 plt.plot(mrktshr_global_pct)
 plt.title('Global Marketshare of Silicon PV installed since 1995')
 plt.ylim(0,1.1)
@@ -116,7 +133,7 @@ plt.ylim(0,1.1)
 # # Marketshare weight PV installs by percent Silicon
 # Now we have a marketshare percentage of silicon for 1995 through 2019. We will multiply the PV installs by this silicon marketshare to get the MW of silicon PV installed globally since 1995.
 
-# In[48]:
+# In[ ]:
 
 
 #put the two dataframes together, joining for available data (excludes NANs)
@@ -136,7 +153,7 @@ plt.title('Silicon PV Installations (MW) Globally, 1995 through 2019')
 plt.legend()
 
 
-# In[49]:
+# In[ ]:
 
 
 world_si_installs.to_csv(cwd+'/../../PV_ICE/baselines/SupportingMaterial/output_Global_SiPV_installs.csv', index=True)
@@ -148,7 +165,7 @@ world_si_installs.to_csv(cwd+'/../../PV_ICE/baselines/SupportingMaterial/output_
 # 
 # This section documents and munges the IRENA world historical PV install data by country. We can then modify it by the world marketshare of silicon.
 
-# In[52]:
+# In[16]:
 
 
 import numpy as np
@@ -161,7 +178,7 @@ cwd = os.getcwd() #grabs current working directory
 supportMatfolder = str(Path().resolve().parent.parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial')
 
 
-# In[56]:
+# In[17]:
 
 
 IRENA_raw_file = os.path.join(supportMatfolder, 'HistoricalCapacityWorld-QueryResult-PVNetAdds.xlsx')
@@ -171,7 +188,7 @@ IRENA_global_raw.drop(columns=list(IRENA_global_raw.filter(like='Unnamed')), inp
 IRENA_global_raw.drop(columns=['Technology','Indicator'], inplace=True) #drop these columns
 
 
-# In[61]:
+# In[18]:
 
 
 IRENA_locs = list(IRENA_global_raw.index)
@@ -179,21 +196,57 @@ regions = ['Africa','Asia','C America + Carib','Eurasia','Europe','EU 27','Middl
 countries = [i for i in IRENA_locs if i not in regions] #leaves in world
 
 
-# In[65]:
+# In[19]:
 
 
 IRENA_global = IRENA_global_raw.T
 
 
-# In[68]:
+# In[20]:
 
 
 IRENA_regions = IRENA_global.loc[:,regions]
 IRENA_countries = IRENA_global.loc[:,countries]
 
 
-# In[70]:
+# In[21]:
 
 
-IRENA_regions
+IRENA_regions.to_csv(path_or_buf=os.path.join(supportMatfolder, 'output-RegionInstalls-alltech.csv'))
+IRENA_countries.to_csv(path_or_buf=os.path.join(supportMatfolder, 'output-CountryInstalls-alltech.csv'))
+
+
+# ## Market Share Weight Global installs by Silicon
+
+# In[25]:
+
+
+mrktshr_global.index[-1] #what is the last year we have data for?
+
+
+# In[26]:
+
+
+#trim the c-Si marketshare to match index of IRENA installs
+mrktshr_cSiglobal_IRENA = mrktshr_global.loc[mrktshr_global.index>1999]/100 #turn into decimal
+
+
+# In[28]:
+
+
+mrktshr_cSiglobal_IRENA.columns
+
+
+# In[31]:
+
+
+IRENA_regions_cSi = IRENA_regions.multiply(mrktshr_cSiglobal_IRENA["Global_MarketShare"], axis="index")
+IRENA_countries_cSi = IRENA_countries.multiply(mrktshr_cSiglobal_IRENA["Global_MarketShare"], axis="index")
+
+
+# In[32]:
+
+
+IRENA_regions_cSi.to_csv(path_or_buf=os.path.join(supportMatfolder, 'output-RegionInstalls-cSi.csv'))
+IRENA_countries_cSi.to_csv(path_or_buf=os.path.join(supportMatfolder, 'output-CountryInstalls-cSi.csv'))
 
