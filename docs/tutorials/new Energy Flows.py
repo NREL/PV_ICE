63e44354479e39eff742f9aaf4f75674251dde0b
heathermirletz <60519205+heathermirletz@users.noreply.github.com>
2023-04-13 10:33:24 -0600

    updated energy flow tutorial
    with the new features, tested the fuel column addition, it works, yay!

--- docs/tutorials/new Energy Flows.py
+++ docs/tutorials/new Energy Flows.py
@@ -5,7 +5,7 @@
 
 # This journal documents and demonstrates the new Energy flows calculation capacity of PV ICE
 
-# In[1]:
+# In[8]:
 
 
 import os
@@ -16,6 +16,7 @@ import pandas as pd
 import numpy as np
 
 testfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'TEMP')
+baselinesfolder = str(Path().resolve().parent.parent /'PV_ICE' / 'baselines')
 
 # Another option using relative address; for some operative systems you might need '/' instead of '\'
 # testfolder = os.path.abspath(r'..\..\PV_ICE\TEMP')  
@@ -23,7 +24,7 @@ testfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'TEMP')
 print ("Your simulation will be stored in %s" % testfolder)
 
 
-# In[2]:
+# In[9]:
 
 
 PV_ICE.__version__
@@ -31,119 +32,62 @@ PV_ICE.__version__
 
 # ### Add Scenarios and Materials
 
-# In[3]:
+# In[10]:
 
 
 cwd=os.getcwd()
 print(os.getcwd())
 
 
-# In[4]:
+# In[11]:
 
 
 MATERIALS = ['glass']#,'aluminium_frames','silver','silicon', 'copper', 'encapsulant', 'backsheet']
 MATERIAL = MATERIALS[0]
 
-moduleFile_linear = r'..\baselines\perovskite_modules_US_linear.csv'
+moduleFile_m = r'..\baselines\baseline_modules_mass_US.csv'
+moduleFile_e = r'..\baselines\baseline_modules_energy.csv'
 
 
-# In[5]:
+# In[13]:
 
 
-r1 = PV_ICE.Simulation(name='perovskite_energies', path=testfolder)
+r1 = PV_ICE.Simulation(name='energy_test', path=testfolder)
 
-r1.createScenario(name='perovskite_linear', file=moduleFile_linear)
+r1.createScenario(name='energy_test', massmodulefile=moduleFile_m, energymodulefile=moduleFile_e)
 for mat in range (0, len(MATERIALS)):
-    MATERIALBASELINE = r'..\baselines\perovskite_material_'+MATERIALS[mat]+'.csv'
-    r1.scenario['perovskite_linear'].addMaterial(MATERIALS[mat], file=MATERIALBASELINE)
+    matbaseline_m = os.path.join(baselinesfolder,'baseline_material_mass_'+MATERIALS[mat]+'.csv')
+    matbaseline_e = os.path.join(baselinesfolder,'baseline_material_energy_'+MATERIALS[mat]+'.csv')
+    r1.scenario['energy_test'].addMaterial(MATERIALS[mat], massmatfile=matbaseline_m, energymatfile=matbaseline_e)
 
 
-# ### Run the Mass Flow Calculations on All Scenarios and Materials
-
-# In[6]:
-
-
-r1.calculateMassFlow()
-
-
-# ## Calculating Energy Flows
-
-# First read in the energy files. Point at a path, then use the PV ICE colde to handle the meta data. 
-# 
-# Energy values for modules are in kWh/m2 and for materials are in kWh/kg. To ensure unit matching, we will divide the input by 1000 to convert kg to g.
-
-# In[7]:
-
-
-matEfile_glass = str(Path().resolve().parent.parent /'PV_ICE' / 'baselines'/'perovskite_energy_material_glass.csv')
-
-modEfile = str(Path().resolve().parent.parent /'PV_ICE' / 'baselines'/'perovskite_energy_modules.csv')
-
-
-# In[8]:
-
-
-file = matEfile_glass
-csvdata = open(str(file), 'r', encoding="UTF-8")
-csvdata = open(str(file), 'r', encoding="UTF-8-sig")
-firstline = csvdata.readline()
-secondline = csvdata.readline()
-
-head = firstline.rstrip('\n').split(",")
-meta = dict(zip(head, secondline.rstrip('\n').split(",")))
-
-data = pd.read_csv(csvdata, names=head)
-data.loc[:, data.columns != 'year'] = data.loc[:, data.columns != 'year'].astype(float)/1000 
-#Divide by 1000 go get kg to g
-# THIS IS A TEMP FIX
-matEfile_glass_simple = data.copy()
-
-
-# In[9]:
-
-
-file = modEfile
-csvdata = open(str(file), 'r', encoding="UTF-8")
-csvdata = open(str(file), 'r', encoding="UTF-8-sig")
-firstline = csvdata.readline()
-secondline = csvdata.readline()
-
-head = firstline.rstrip('\n').split(",")
-meta = dict(zip(head, secondline.rstrip('\n').split(",")))
-
-data = pd.read_csv(csvdata, names=head)
-data.loc[:, data.columns != 'year'] = data.loc[:, data.columns != 'year'].astype(float)
-modEfile_simple = data.copy()
-
-
-# Now run the energy calculation. Currently this is not a class, just a function that will return a dataframe. Each scenario will need to be run seperately, and read in the perovskite energy files.
-
-# In[10]:
-
-
-r1_e_linear = r1.calculateEnergyFlow(scenarios='perovskite_linear', materials='glass', modEnergy=modEfile_simple, matEnergy=matEfile_glass_simple)
-
-
-# ## Energy Analysis
-
-# In[11]:
-
-
-r1_e_linear.keys()
-
+# # Calculate flows
 
 # In[14]:
 
 
-r1_e_linear.index = modEfile_simple['year'] #temp fix to reassign years to this dataframe
+r1.calculateFlows()
 
+
+# Now run the energy calculation. Currently this is not a class, just a function that will return a dataframe. Each scenario will need to be run seperately, and read in the perovskite energy files.
+
+# ## Energy Analysis
 
 # In[15]:
 
 
-for key in r1_e_linear.keys():
-    plt.plot(r1_e_linear.index, r1_e_linear[key], label='linear')
-    plt.legend()
-    plt.title(str(key))
-    plt.show()
+allenergy, energyGen, energy_demands = r1.aggregateEnergyResults()
+
+
+# In[19]:
+
+
+energy_demands#.filter(like='_fuel')
+energy_demands.columns
+
+
+# In[20]:
+
+
+energy_demands['energy_test_glass_mat_MFG_virgin_fuel']/energy_demands['energy_test_glass_mat_MFG_virgin']
 
