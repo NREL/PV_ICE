d32fee4dd8bf4f0b89ad507b300cc12c888d0944
Silvana Ovaitt <silvana.ayala@nrel.gov>
2023-10-23 21:52:04 -0600

    1 - Basic run

--- training/tutorials/1 - Basic run.py
+++ training/tutorials/1 - Basic run.py
@@ -8,14 +8,16 @@
 # 
 # **Objectives:**
 # 1. Read in necessary mass and energy data
-# 2. Modify necessary values manually and programatically
+# 2. Run
 # 3. Generate plots 
 
-# In[1]:
+# ## 1. Setup and Create PV ICE Simulation Object
+
+# In[ ]:
 
 
 # if running on google colab, uncomment the next line and execute this cell to install the dependencies and prevent "ModuleNotFoundError" in later cells:
-# !pip install git+https://github.com/NREL/PV_ICE.git@development
+get_ipython().system('pip install git+https://github.com/NREL/PV_ICE.git@development')
 
 
 # In[ ]:
@@ -23,26 +25,6 @@
 
 import PV_ICE
 PV_ICE.__version__
-
-
-# In[ ]:
-
-
-from google.colab import files
-files.upload()
-
-
-# In[ ]:
-
-
-import os
-os.listdir()
-
-
-# In[ ]:
-
-
-pwd
 
 
 # In[ ]:
@@ -63,6 +45,14 @@ r1 = PV_ICE.Simulation(name='Sim1', path=testfolder); # Is it possible to define
 # In[ ]:
 
 
+r1
+
+
+# PV ICE uses <i>object oriented programing</i>, so this created a ``PV_ICE.main.Simulation`` Object. An Object is data field that has unique attributes and behaviors. We can crack open objects with the `.__dict__` command
+
+# In[ ]:
+
+
 r1.__dict__
 
 
@@ -72,16 +62,47 @@ r1.__dict__
 r1.baselinepath = '/content'
 
 
+# ## 2. Add the Files to the Folder now
+# 
+# Drag the files to the `/content` folder 
+# 
+# Alternatively, the cell below can open a upload window
+
 # In[ ]:
 
 
-r1.createScenario(name='standard', massmodulefile=r'baseline_modules_mass_US.csv', energymodulefile = 'baseline_modules_energy.csv' )
+#from google.colab import files
+#files.upload()
 
+
+# ## 3. Create your Scenario
+# 
+# First create scenario and add its module files 
 
 # In[ ]:
 
 
-r1.scenario['standard'].addMaterial('glass', massmatfile='/content/baseline_material_mass_glass.csv', energymatfile='/content/baseline_material_energy_glass.csv')
+r1.createScenario(name='standard', 
+                  massmodulefile=r'baseline_modules_mass_US.csv', 
+                  energymodulefile = 'baseline_modules_energy.csv' )
+
+
+# Then add at least one material file
+
+# In[ ]:
+
+
+r1.scenario['standard'].addMaterial(materialname='glass', 
+                                    massmatfile='/content/baseline_material_mass_glass.csv', 
+                                    energymatfile='/content/baseline_material_energy_glass.csv')
+
+
+# We can explore the module and material objects now. For example:
+
+# In[ ]:
+
+
+r1.scenario['standard'].material['glass']
 
 
 # In[ ]:
@@ -96,12 +117,21 @@ r1.scenario['standard'].material['glass'].__dict__
 r1.scenario['standard'].material['glass'].matdataIn_m
 
 
+# ## 4. Run the flows 
+
 # In[ ]:
 
 
 r1.calculateMassFlow()
+
+
+# In[ ]:
+
+
 r1.calculateEnergyFlow()
 
+
+# Same thing, but together 
 
 # In[ ]:
 
@@ -109,41 +139,49 @@ r1.calculateEnergyFlow()
 r1.calculateFlows()
 
 
-# In[8]:
+# We can explore the outputs. They are still a bit raw at this step
+
+# In[ ]:
 
 
 r1.scenario['standard'].dataOut_m.keys()
 
 
-# What can aggregate results from dataOut_m and matdataOut_m and compile the data so we can use it more easily
+# ## 5. Aggregate Results
 
-# In[9]:
+# In[ ]:
 
 
 USyearly, UScum = r1.aggregateResults()
+
+# The results are also saved into the simulation object itself:
 # r1.USyearly
 # r1.UScum
 
 
-# In[10]:
+# In[ ]:
 
 
 USyearly.keys()
 
 
-# In[11]:
+# In[ ]:
 
 
 UScum.keys()
 
 
-# In[12]:
+# ## 6. Save Simulation
+# 
+# This is useful to see the inputs (in case you modified the values with some of the support functions), the full dataframes of internal processing data, and the outputs in an organized manner
+
+# In[ ]:
 
 
 r1.saveSimulation()
 
 
-# ### Step 5: Use internal plotting functions to plot results
+# ## 7. Use internal plotting functions to plot results
 
 # From this list, select the one that fits your study and select the type of plotting method. There are various plotting options:
 # * `plotScenariosComparison`
@@ -154,38 +192,32 @@ r1.saveSimulation()
 
 # You can also view all the keywords you can use by calling the function without argumnets, or by printing the keys to the module data or the material data
 
-# In[13]:
-
-
-r1.plotScenariosComparison()
-
-
-# In[14]:
-
-
-r1.plotMaterialComparisonAcrossScenarios(material='silicon', keyword='mat_virgin_eff')
-
-
-# In[15]:
-
-
-r1.plotMaterialResults(keyword='VirginStock')
-
-
-# In[16]:
+# In[ ]:
 
 
 r1.plotMetricResults()
 
 
-# In[17]:
+# In[ ]:
 
 
-r1.plotInstalledCapacityResults()
+r1.plotScenariosComparison()
 
 
 # In[ ]:
 
 
+r1.plotMaterialComparisonAcrossScenarios(material='silicon', keyword='mat_virgin_eff')
 
+
+# In[ ]:
+
+
+r1.plotMaterialResults(keyword='VirginStock')
+
+
+# In[ ]:
+
+
+r1.plotInstalledCapacityResults()
 
