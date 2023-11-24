a1540cddbaace86901857ad03a2456c4b8215e84
Silvana Ovaitt <silvana.ayala@nrel.gov>
2023-11-24 14:41:55 -0700

    cleanup tut 3

--- docs/tutorials/3 - Advanced - Explore Repair and Reuse.py
+++ docs/tutorials/3 - Advanced - Explore Repair and Reuse.py
@@ -29,13 +29,25 @@ import PV_ICE
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Parameters for plots.
 plt.rcParams.update({'font.size': 22})
 plt.rcParams['figure.figsize'] = (12, 5)
 
 
+# In[4]:
+
+
+# This information helps with debugging and getting support :)
+import sys, platform
+print("Working on a ", platform.system(), platform.release())
+print("Python version ", sys.version)
+print("Pandas version ", pd.__version__)
+print("PV_ICE version ", PV_ICE.__version__)
+
+
 # ## REPAIR
 
-# In[6]:
+# In[5]:
 
 
 r1 = PV_ICE.Simulation(name='Simulation1', path=testfolder)
@@ -61,7 +73,7 @@ r1.scenario['Repair_50'].addMaterial('silicon',
                                      energymatfile=r'..\..\baselines\baseline_material_energy_silicon.csv')
 
 
-# In[7]:
+# In[6]:
 
 
 r1.scenario['Repair_0'].dataIn_m['mod_Repair'] = 0
@@ -77,10 +89,10 @@ r1.scenario['Repair_0'].dataIn_m['mod_lifetime'] = 50
 r1.scenario['Repair_50'].dataIn_m['mod_lifetime'] = 50
 
 
-# In[8]:
+# In[27]:
 
 
-r1.calculateMassFlow()
+r1.calculateMassFlow(debugflag=True) # Turning debug flag to True to explore internal yearly effects later on.
 r1.calculateEnergyFlow(insolation=4800) # Modify to European value here. 4800 is for US NSRDB Average.
 
 
@@ -93,19 +105,7 @@ r1.calculateEnergyFlow(insolation=4800) # Modify to European value here. 4800 is
 # * r1.scenario['Repair_0'].material['glass'].matdataOut_m.keys()
 # * r1.scenario['Repair_0'].material['glass'].matdataOut_e.keys()
 
-# In[9]:
-
-
-r1.scenario['Repair_0'].material['glass'].matdataOut_m.keys()
-
-
-# In[10]:
-
-
-r1.scenario['Repair_0'].material['glass'].matdataIn_m
-
-
-# In[11]:
+# In[28]:
 
 
 USyearly, UScum = r1.aggregateResults()
@@ -113,33 +113,33 @@ USyearly.to_csv('USYearly.csv')
 UScum.to_csv('USCum.csv')
 
 
-# In[13]:
+# In[29]:
 
 
 r1.saveSimulation()
 
 
-# In[14]:
+# In[30]:
 
 
 r1.plotMaterialComparisonAcrossScenarios(keyword='mat_Manufacturing_Input', material='glass')
 
 
-# # clean after this
+# ## Plotting
 
-# In[ ]:
-
-
-AREA = r1.scenario['Repair_0'].data['Area'].iloc[0]
+# In[31]:
 
 
-# In[ ]:
+AREA = r1.scenario['Repair_0'].dataOut_m['Area'].iloc[0]
 
 
-filter_col = [col for col in r1.scenario['Repair_0'].data if col.startswith('EOL_on_Year_')]
+# In[44]:
 
 
-# In[ ]:
+filter_col = [col for col in r1.scenario['Repair_0'].dataDebug_m if 'EOL_L0' in col]
+
+
+# In[47]:
 
 
 Cumul_EOL_R0 = []
@@ -148,19 +148,19 @@ Cumul_EOL_R50 = []
 foo=0
 foo2=0
 for life in range (0, len(filter_col)):
-    foo +=  r1.scenario['Repair_0'].data[filter_col[life]].iloc[0]
-    foo2 += r1.scenario['Repair_50'].data[filter_col[life]].iloc[0]
+    foo +=  r1.scenario['Repair_0'].dataDebug_m[filter_col[life]].iloc[0]
+    foo2 += r1.scenario['Repair_50'].dataDebug_m[filter_col[life]].iloc[0]
     Cumul_EOL_R0.append(foo)
     Cumul_EOL_R50.append(foo2)
 
 
-# In[ ]:
+# In[49]:
 
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 plt.plot(AREA-Cumul_EOL_R0, label='Repair = 0%')
-plt.plot(AREA-Cumul_EOL_R50, label='Repair = 0%')
+plt.plot(AREA-Cumul_EOL_R50, label='Repair = 50%')
 plt.ylabel('Working Area [m$^2$]')
 plt.xlabel('Years of Service')
 plt.title('Generation 1995 - Repair effects')
@@ -170,7 +170,7 @@ plt.xlim([0,45])
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 plt.plot(AREA-Cumul_EOL_R0, label='Repair = 0%')
-plt.plot(AREA-Cumul_EOL_R50, label='Repair = 0%')
+plt.plot(AREA-Cumul_EOL_R50, label='Repair = 50%')
 ax.set_yscale('log')
 plt.ylabel('Working Area [m$^2$]')
 plt.xlabel('Years of Service')
@@ -179,36 +179,38 @@ plt.legend()
 plt.xlim([0,45])
 
 
-# In[ ]:
+# In[51]:
 
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-plt.plot(Cumul_EOL_R0)
-plt.plot(Cumul_EOL_R50)
+plt.plot(Cumul_EOL_R0, label='Repair = 0%')
+plt.plot(Cumul_EOL_R50, label='Repair = 0%')
 plt.ylabel('Area Disposed [m$^2$]')
 plt.xlabel('Years of Service')
 plt.title('Generation 1995 - Repair effects')
+plt.legend()
 plt.xlim([0,45])
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-plt.plot(Cumul_EOL_R0)
-plt.plot(Cumul_EOL_R50)
+plt.plot(Cumul_EOL_R0, label='Repair = 0%')
+plt.plot(Cumul_EOL_R50, label='Repair = 0%')
 ax.set_yscale('log')
 plt.ylabel('Area Disposed [m$^2$]')
 plt.xlabel('Years of Service')
 plt.title('Generation 1995 - Repair effects')
+plt.legend()
 plt.xlim([0,45])
 
 
-# In[ ]:
+# In[54]:
 
 
-r1.plotScenariosComparison(keyword='Installed_Capacity_[W]')
+r1.plotScenariosComparison('Effective_Capacity_[W]')
 
 
-# In[ ]:
+# In[55]:
 
 
 r1.plotMaterialComparisonAcrossScenarios(material='glass', keyword='mat_Total_Landfilled')
@@ -219,61 +221,60 @@ r1.plotMaterialComparisonAcrossScenarios(material='silicon', keyword='mat_Total_
 # 
 # Starting a Clean simulation
 
-# In[ ]:
+# In[60]:
 
 
 r1 = PV_ICE.Simulation(name='Simulation1', path=testfolder)
-r1.createScenario(name='Reuse_0', file=r'..\baselines\baseline_modules_US.csv')
-r1.scenario['Reuse_0'].addMaterial('glass', file=r'..\baselines\baseline_material_glass.csv')
-r1.scenario['Reuse_0'].addMaterial('silicon', file=r'..\baselines\baseline_material_silicon.csv')
+r1.createScenario(name='Reuse_0', massmodulefile=r'baseline_modules_mass_US.csv')
+r1.scenario['Reuse_0'].addMaterial('glass', massmatfile=r'..\..\baselines\baseline_material_mass_glass.csv')
+r1.scenario['Reuse_0'].addMaterial('silicon', massmatfile=r'..\..\baselines\baseline_material_mass_silicon.csv')
 
-r1.createScenario(name='Reuse_50', file=r'..\baselines\baseline_modules_US.csv')
-r1.scenario['Reuse_50'].addMaterial('glass', file=r'..\baselines\baseline_material_glass.csv')
-r1.scenario['Reuse_50'].addMaterial('silicon', file=r'..\baselines\baseline_material_silicon.csv')
-
-
-# In[ ]:
+r1.createScenario(name='Reuse_50', massmodulefile=r'baseline_modules_mass_US.csv')
+r1.scenario['Reuse_50'].addMaterial('glass', massmatfile=r'..\..\baselines\baseline_material_mass_glass.csv')
+r1.scenario['Reuse_50'].addMaterial('silicon', massmatfile=r'..\..\baselines\baseline_material_mass_silicon.csv')
 
 
-r1.scenario['Reuse_0'].data['mod_Reuse'] = 0
-r1.scenario['Reuse_50'].data['mod_Reuse'] = 50
-r1.scenario['Reuse_50'].data['mod_Reuse'] = 50
+# In[73]:
 
-r1.scenario['Reuse_0'].data['mod_reliability_t50'] = 35
-r1.scenario['Reuse_0'].data['mod_reliability_t90'] = 45
-r1.scenario['Reuse_50'].data['mod_reliability_t50'] = 35
-r1.scenario['Reuse_50'].data['mod_reliability_t90'] = 45
+
+r1.scenario['Reuse_0'].dataIn_m.keys()
+
+
+# In[74]:
+
+
+r1.scenario['Reuse_0'].dataIn_m['mod_Reuse'] = 0
+r1.scenario['Reuse_50'].dataIn_m['mod_EOL_pg0_resell'] = 50
+
+r1.scenario['Reuse_0'].dataIn_m['mod_reliability_t50'] = 35
+r1.scenario['Reuse_0'].dataIn_m['mod_reliability_t90'] = 45
+r1.scenario['Reuse_50'].dataIn_m['mod_reliability_t50'] = 35
+r1.scenario['Reuse_50'].dataIn_m['mod_reliability_t90'] = 45
 
 # Setting Project Lifetime beyond Failures
-r1.scenario['Reuse_0'].data['mod_lifetime'] = 25
-r1.scenario['Reuse_50'].data['mod_lifetime'] = 25
+r1.scenario['Reuse_0'].dataIn_m['mod_lifetime'] = 25
+r1.scenario['Reuse_50'].dataIn_m['mod_lifetime'] = 25
 
 
-# In[ ]:
+# In[75]:
 
 
-r1.calculateMassFlow()
+r1.calculateMassFlow(debugflag=True)
 
 
-# In[ ]:
+# In[76]:
 
 
-r1.scenario['Reuse_50'].data.keys()
+AREA = r1.scenario['Reuse_50'].dataOut_m['Area'].iloc[0]
 
 
-# In[ ]:
+# In[77]:
 
 
-AREA = r1.scenario['Reuse_50'].data['Area'].iloc[0]
+filter_col = [col for col in r1.scenario['Reuse_50'].dataDebug_m if 'EOL_L0' in col]
 
 
-# In[ ]:
-
-
-filter_col = [col for col in r1.scenario['Reuse_50'].data if col.startswith('EOL_on_Year_')]
-
-
-# In[ ]:
+# In[78]:
 
 
 Cumul_EOL_R0 = []
@@ -282,27 +283,13 @@ Cumul_EOL_R50 = []
 foo=0
 foo2=0
 for life in range (0, len(filter_col)):
-    foo +=  r1.scenario['Reuse_0'].data[filter_col[life]].iloc[0]
-    foo2 += r1.scenario['Reuse_50'].data[filter_col[life]].iloc[0]
+    foo +=  r1.scenario['Reuse_0'].dataDebug_m[filter_col[life]].iloc[0]
+    foo2 += r1.scenario['Reuse_50'].dataDebug_m[filter_col[life]].iloc[0]
     Cumul_EOL_R0.append(foo)
     Cumul_EOL_R50.append(foo2)
 
 
-# In[ ]:
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-plt.plot(AREA-Cumul_EOL_R0, label='Reuse = 0%')
-#plt.plot(AREA-Cumul_EOL_R50, label='Reuse = 50%')
-plt.ylabel('Working Area [m$^2$]')
-plt.xlabel('Years of Service')
-plt.title('Generation 1995 - Reuse at End of Project Lifetime')
-plt.legend()
-plt.xlim([0,45])
-
-
-# In[ ]:
+# In[80]:
 
 
 fig = plt.figure()
@@ -327,7 +314,7 @@ plt.title('Generation 1995 - Reuse at End of Project Lifetime')
 plt.xlim([0,45])
 
 
-# In[ ]:
+# In[81]:
 
 
 fig = plt.figure()
@@ -352,27 +339,8 @@ plt.legend()
 plt.xlim([0,45])
 
 
-# In[ ]:
+# In[83]:
 
 
-r1.plotScenariosComparison(keyword='Installed_Capacity_[W]')
-
-
-# In[ ]:
-
-
-r1.plotMaterialComparisonAcrossScenarios(material='glass', keyword='mat_Total_Landfilled')
-r1.plotMaterialComparisonAcrossScenarios(material='silicon', keyword='mat_Total_Landfilled')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+r1.plotScenariosComparison(keyword='Effective_Capacity_[W]')
 
