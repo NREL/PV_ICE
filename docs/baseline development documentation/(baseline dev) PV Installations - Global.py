a33c8ca78e6e2bfcd0912ecd3ef71492c892ff7a
Mirletz <heather.mirletz@nrel.gov>
2024-04-29 16:29:24 -0600

    Update global installs
    Current global deployment is Silicon only! Also updated T50, T90, Lifetime, Degradation to match updated UST50T90 Baseline

--- docs/baseline development documentation/(baseline dev) PV Installations - Global.py
+++ docs/baseline development documentation/(baseline dev) PV Installations - Global.py
@@ -13,40 +13,44 @@ import pandas as pd
 import os,sys
 from pathlib import Path
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 28})
-plt.rcParams['figure.figsize'] = (30, 15)
+
 
 cwd = os.getcwd() #grabs current working directory
 
 supportMatfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial')
 
 
-# In[2]:
+# In[52]:
 
 
 cwd = os.getcwd() #grabs current working directory
-df_installs_raw = pd.read_csv(os.path.join(supportMatfolder,'PVInstalls_World_AllSources.csv'), index_col='Year')
+df_installs_raw = pd.read_csv(os.path.join(supportMatfolder,'PVInstalls_World_AllSources.csv'), index_col='Year', skiprows=[1])
 sources = df_installs_raw.columns
 print(len(sources))
 
 
-# In[3]:
+# In[53]:
 
 
 df_installs_raw
 
 
-# In[4]:
+# In[54]:
 
+
+plt.rcParams.update({'font.size': 16})
+plt.rcParams['figure.figsize'] = (10,6)
 
 plt.plot(df_installs_raw.index,df_installs_raw[sources[0]],lw=4,marker='*',label=sources[0])
 plt.plot(df_installs_raw.index,df_installs_raw[sources[1]],lw=3,marker='o',label=sources[1])
 plt.plot(df_installs_raw.index,df_installs_raw[sources[2]],lw=2,marker='o',label=sources[2])
 plt.plot(df_installs_raw.index,df_installs_raw[sources[3]],lw=2,marker='o',label=sources[3])
+plt.plot(df_installs_raw.index,df_installs_raw[sources[4]],lw=2,marker='o',label=sources[4])
+
 
 plt.yscale('log')
 plt.ylabel('PV Installed (MW)')
-plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left")
+plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", fontsize=12)
 #plt.plot(df_installs_raw, marker='o')
 
 
@@ -54,12 +58,12 @@ plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left")
 
 # Based on the above graph, we will utilize Goetzberger data through 2000, then IRENA online query tool after 2000.
 
-# In[5]:
+# In[55]:
 
 
 #Before 2000 = Goetz
 installs_old = df_installs_raw.loc[(df_installs_raw.index<=2000) & (df_installs_raw.index>=1995)]
-installs_old_Goetz = pd.DataFrame(installs_old[sources[3]])
+installs_old_Goetz = pd.DataFrame(installs_old[sources[4]])
 installs_old_Goetz.columns = ['installed_pv_MW']
 
 #After 2000 = IRENA
@@ -71,7 +75,7 @@ installs_recent_IRENA.columns = ['installed_pv_MW']
 
 # ### Collect the installation data together into a single df
 
-# In[6]:
+# In[56]:
 
 
 installs = pd.concat([installs_old_Goetz,installs_recent_IRENA])
@@ -80,13 +84,7 @@ plt.yscale('log')
 plt.title('Installations of PV Globally (MW) since 1995')
 
 
-# In[ ]:
-
-
-
-
-
-# In[7]:
+# In[57]:
 
 
 installs.to_csv(os.path.join(supportMatfolder, 'output_Global_allPV_installs.csv'))
@@ -96,7 +94,7 @@ installs.to_csv(os.path.join(supportMatfolder, 'output_Global_allPV_installs.csv
 
 # In addition to compiling a single installation record for 1995 through the present, this data is total cumulative, but the tool it currently considering crystalline silicon technology only (i.e. mono and multi, but not ribbon or amorphous).
 
-# In[8]:
+# In[58]:
 
 
 cwd = os.getcwd() #grabs current working directory
@@ -105,7 +103,7 @@ refs = df_raw_mrktshr_siVtf.columns
 print(len(refs))
 
 
-# In[9]:
+# In[59]:
 
 
 plt.rcParams.update({'font.size': 14})
@@ -118,25 +116,31 @@ plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left")
 plt.ylim(50,100)
 
 
-# The 2020 Fraunhofer and 2014 Fraunhofer appear to agree reasonably closely, and Mints agrees closely for the amount of time there is overlap. The unknown sourced wikipedia figure doesn't agree until 2010, but given the unknown source, it will be discarded. We will use the Fraunhofer ISE 2020 market share data for the entire time period.
+# The 2020 Fraunhofer and 2014 Fraunhofer appear to agree reasonably closely, and Mints agrees closely for the amount of time there is overlap. The unknown sourced wikipedia figure doesn't agree until 2010, but given the unknown source, it will be discarded. We will use the Fraunhofer ISE marketshare data pre-2008, and Mints marketshare post 2008.
 
-# In[10]:
+# In[60]:
 
 
 refs
 
 
-# In[11]:
+# In[61]:
 
 
-df_mrktshr_global =  pd.DataFrame(df_raw_mrktshr_siVtf[refs[2]]) #select Fraunhofer for historical 1995-2020
-mrktshr_global = df_mrktshr_global.loc[(df_mrktshr_global.index>=1995)]
-# use PMints for most recent data
-mrktshr_global.columns = ['Global_MarketShare']
-#print(mrktshr_global)
+#Before 2008 = Fraunhofer
+mrktshareSi_global_old = pd.DataFrame(df_raw_mrktshr_siVtf.loc[(df_raw_mrktshr_siVtf.index<=2007),refs[2]])
+mrktshareSi_global_old.columns = ['Global_MarketShare']
+#mrktshare_global_old
+
+#After 2008 = Mints
+marketshareSi_global_new = pd.DataFrame(df_raw_mrktshr_siVtf.loc[(df_raw_mrktshr_siVtf.index>2007),refs[1]])
+marketshareSi_global_new.columns = ['Global_MarketShare']
+
+mrktshr_global = pd.concat([mrktshareSi_global_old,marketshareSi_global_new])
+#mrktshr_global
 
 
-# In[12]:
+# In[62]:
 
 
 #convert to decimal
@@ -151,13 +155,7 @@ plt.ylim(0,1.1)
 # # Marketshare weight PV installs by percent Silicon
 # Now we have a marketshare percentage of silicon for 1995 through 2019. We will multiply the PV installs by this silicon marketshare to get the MW of silicon PV installed globally since 1995.
 
-# In[ ]:
-
-
-
-
-
-# In[13]:
+# In[63]:
 
 
 #put the two dataframes together, joining for available data (excludes NANs)
@@ -166,8 +164,12 @@ df = pd.concat(dfs, axis=1, join='inner')
 df_clean = df.dropna()
 #creates the marketshare weighted c-Si install data
 world_si_installs = df_clean.agg('prod', axis='columns') 
+#world_si_installs
 
-#print(us_si_installs)
+
+# In[64]:
+
+
 plt.rcParams.update({'font.size': 18})
 plt.rcParams['figure.figsize'] = (15, 8)
 plt.plot(installs, label='All Global PV Installed', color='orange')
@@ -177,21 +179,11 @@ plt.title('Silicon PV Installations (MW) Globally, 1995 through 2019')
 plt.legend()
 
 
-# In[14]:
+# In[65]:
 
 
-cwd = os.getcwd() #grabs current working directory
-cwd
+world_si_installs.to_csv(os.path.join(supportMatfolder, 'output_Global_SiPV_installs.csv'))
 
-
-# In[15]:
-
-
-#world_si_installs.to_csv(cwd+'/../../PV_ICE/baselines/SupportingMaterial/output_Global_SiPV_installs.csv', index=True)
-world_si_installs.to_csv(os.path.join(supportMatfolder, 'output_Global_allPV_installs.csv'))
-
-
-# This data only takes installs through 2019. For the remaining years through 2050, a compound annual growth rate of 8.9% was used to predict increasing installations. This compound annual growth rate was sourced from IRENA 2016 EoL Mangement Report and IRENA 2019 Future of PV Report.
 
 # # Global Installations by Country
 # 
@@ -260,7 +252,7 @@ IRENA_regions.to_csv(path_or_buf=os.path.join(supportMatfolder, 'output-RegionIn
 IRENA_countries.to_csv(path_or_buf=os.path.join(supportMatfolder, 'output-CountryInstalls-alltech.csv'))
 
 
-# ## Market Share Weight Global installs by Silicon
+# ### Market Share Weight Countries installs by Silicon
 
 # In[24]:
 
