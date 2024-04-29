edc613606d9528f5ae7500ab671db349bcd40e85
Mirletz <heather.mirletz@nrel.gov>
2024-04-29 17:24:15 -0600

    updating module efficiency

--- docs/baseline development documentation/(baseline development) Module Efficiency.py
+++ docs/baseline development documentation/(baseline development) Module Efficiency.py
@@ -3,30 +3,34 @@
 
 # # Module Efficiency History and Projections
 
-# In[1]:
+# In[3]:
 
 
 import numpy as np
 import pandas as pd
 import os,sys
+from pathlib import Path
 import matplotlib.pyplot as plt
+
+cwd = os.getcwd() #grabs current working directory
+supportMatfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial')
+
 plt.rcParams.update({'font.size': 22})
 plt.rcParams['figure.figsize'] = (12, 8)
 
 
 # This journal covers the development of a historical baseline and baseline future projection of average module efficiency for each installation year.
 
-# In[2]:
+# In[67]:
 
 
-cwd = os.getcwd() #grabs current working directory
 skipcols = ['Source']
-mod_eff_raw = pd.read_csv(cwd+"/../../../PV_ICE/baselines/SupportingMaterial/module_eff.csv", 
+mod_eff_raw = pd.read_csv(os.path.join(supportMatfolder,"module_eff.csv"), 
                           index_col='Year', usecols=lambda x: x not in skipcols)
 mod_eff_raw['mod_eff'] = pd.to_numeric(mod_eff_raw['mod_eff'])
 
 
-# In[3]:
+# In[68]:
 
 
 print(mod_eff_raw['mod_eff'][2019])
@@ -35,7 +39,7 @@ plt.plot(mod_eff_raw, marker='o')
 
 # There appears to be an "outlier" in 2003. This is from a different source. It does however, fit within the range of module efficiency specified in the prior data point (2001, avg = 13.6, min = 12, max = 16.1). For the purposes of interpolation, we will drop this single datapoint.
 
-# In[4]:
+# In[69]:
 
 
 mod_eff_raw['mod_eff'][2003]=np.nan
@@ -44,16 +48,30 @@ plt.plot(mod_eff_raw, marker='o')
 
 # Now interpolate for missing years. Going to break into 2 parts for this, a linear historical part, and an exponential decay out to 2050.
 
-# In[5]:
+# In[70]:
 
 
-mod_eff_early = mod_eff_raw.loc[(mod_eff_raw.index<=2019)]
+df_findrecentyear = mod_eff_raw.loc[(mod_eff_raw.index<=2049)] #remove goal point
+recentyear = df_findrecentyear.last_valid_index() #find most recent year of efficiency data
+print('The most recent efficiency data is from ',recentyear)
+
+
+# In[71]:
+
+
+mod_eff_early = mod_eff_raw.loc[(mod_eff_raw.index<=recentyear)]
 mod_eff_history = mod_eff_early.interpolate(method='linear',axis=0)
+
+
+# In[72]:
+
+
 #print(mod_eff_history)
 plt.plot(mod_eff_history)
+plt.ylim(0,30)
 
 
-# In[6]:
+# In[73]:
 
 
 # Import curve fitting package from scipy
@@ -63,18 +81,18 @@ def power_law(x, a, b):
     return a*np.power(x, b)
 
 
-# In[7]:
+# In[76]:
 
 
 #generae a dataset for the area in between
-mod_eff_late = mod_eff_raw.loc[(mod_eff_raw.index>=2020)]
-y_dummy = power_law(mod_eff_late.index-2019, mod_eff_late['mod_eff'][2020], 0.065) #17.9
+mod_eff_late = mod_eff_raw.loc[(mod_eff_raw.index>=recentyear)]
+y_dummy = power_law(mod_eff_late.index-(recentyear-1), mod_eff_late['mod_eff'][recentyear], 0.0511) #17.9
 #played around with the exponential until y_dummy[31] closely matched projected 25.06% value. CITE
-print(y_dummy[30])
+print(y_dummy[-1])
 plt.plot(y_dummy)
 
 
-# In[8]:
+# In[77]:
 
 
 #create a dataframe of the projection
@@ -86,30 +104,37 @@ plt.plot(mod_eff_late)
 
 # Now smash the two dataframes back together for our average module efficiency baseline.
 
-# In[9]:
+# In[78]:
 
 
-mod_eff = pd.concat([mod_eff_history, mod_eff_late])
-mod_eff.to_csv(cwd+'/../../../PV_ICE/baselines/SupportingMaterial/output_avg_module_eff_final.csv', index=True)
+mod_eff = pd.concat([mod_eff_history, mod_eff_late.loc[mod_eff_late.index>recentyear]])
+mod_eff.to_csv(os.path.join(supportMatfolder, 'output_avg_module_eff_final.csv'), index=True)
+
+
+# In[59]:
+
+
+plt.plot(mod_eff_raw, marker='o')
 plt.plot(mod_eff)
 plt.title('Average Module Efficiency (%)')
 plt.ylabel('Efficiency (%)')
 
 
-# In[14]:
+# In[66]:
 
 
 #graph for paper
 plt.rcParams.update({'font.size': 22})
 plt.rcParams['figure.figsize'] = (12, 8)
 
-plt.axvspan(2021, 2050.5, facecolor='gray', alpha=0.1)
+plt.axvspan(recentyear+1, 2050.5, facecolor='gray', alpha=0.1)
 plt.plot(mod_eff_raw, marker='o', label='Raw Data')
 plt.plot(mod_eff, '--k', label='PV ICE Baseline')
 plt.title('Average Module Efficiency [%]')
 plt.ylabel('Efficiency [%]')
 plt.legend()
 plt.xlim([1974, 2050.5])
+plt.ylim(0.0, 30)
 plt.legend(frameon=False, bbox_to_anchor=(1.05, 1.0), loc='upper left')
 
 
