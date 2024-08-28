813ecc564a62e26a83e71bf8df8fc90c3350fda1
Mirletz <heather.mirletz@nrel.gov>
2024-08-28 09:14:21 -0600

    CdTe MFGing by country and SIPS
    also some other carbon update. Completed CdTe mfging by country to the best of current ability. Ready to start doing CdTe carbon calcs.

---
+++ Studies/(dev) SIPS Repowering - Test1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+import os,sys
+from pathlib import Path
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+import PV_ICE
+
+cwd = os.getcwd() #grabs current working directory
+
+testfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'TEMP' / 'MESC-NRELStdScens')
+inputfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines'/'NRELStdScenarios')
+baselinesfolder = str(Path().resolve().parent.parent /'PV_ICE' / 'baselines')
+supportMatfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial')
+#altBaselinesfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines' / 'Energy_CellModuleTechCompare')
+
+if not os.path.exists(testfolder):
+    os.makedirs(testfolder)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
