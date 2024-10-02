dede719f56916b5f808ff267e1fb9ee047a7e1e5
Mirletz <heather.mirletz@nrel.gov>
2024-10-02 16:50:31 -0600

    working on CdTe material composition graph
    baselines read in, need to divide and graph.

---
+++ docs/baseline development documentation/(baseline dev) CdTe Material Composition.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CdTe Annual % Material Composition
+# This journal graphs the average composition of a CdTe module over time.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import os,sys
+from pathlib import Path
+import matplotlib.pyplot as plt
+from urllib.request import urlretrieve
+import glob
+#import PV_ICE
+cwd = os.getcwd() #grabs current working directory
+
+
+# In[2]:
+
+
+supportMatfolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines' / 'SupportingMaterial')
+baselinesFolder = str(Path().resolve().parent.parent / 'PV_ICE' / 'baselines')
+cwd
+
+
+# In[3]:
+
+
+#print("Working on a ", platform.system(), platform.release())
+print("Python version ", sys.version)
+print("Pandas version ", pd.__version__)
+print("pyplot ", plt.matplotlib.__version__)
+#print("PV_ICE version ", PV_ICE.__version__)
+
+
+# In[31]:
+
+
+#materials in CdTe
+MATERIALS_CdTe = ['aluminium_frames_cdte','encapsulant_cdte','glass_cdte','copper_cdte','cadmium','tellurium']
+tidynameMats_CdTe = ['aluminium_frames','encapsulant','glass','copper','cadmium','tellurium']
+
+
+# In[21]:
+
+
+pd.read_csv(os.path.join(baselinesFolder, 'baseline_material_mass_cadmium.csv'), 
+            index_col = 0, usecols=['year','mat_massperm2'], skiprows=[1])
+
+
+# In[28]:
+
+
+df_component_mats = pd.DataFrame()
+for mat in MATERIALS_CdTe:
+    filename = os.path.join(baselinesFolder, 'baseline_material_mass_'+str(mat)+'.csv')
+    tempdf = pd.read_csv(filename, index_col = 0, usecols=['year','mat_massperm2'], skiprows=[1])
+    df_component_mats = pd.concat([df_component_mats,tempdf], axis=1)
+    
+
+
+# In[32]:
+
+
+df_component_mats.columns = tidynameMats_CdTe
+df_component_mats
+
+
+# In[33]:
+
+
+df_component_mats['module_mass_gpm2'] = df_component_mats.sum(axis=1) #run only once
+
+
+# In[38]:
+
+
+df_component_mats/df_component_mats['module_mass_gpm2'].values
+
+
+# In[ ]:
+
+
+
+
